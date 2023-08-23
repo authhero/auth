e8096a785330c4d52a896a13f039a5258c24b57c
Daniel Gent <dan@danielgent.com>
2023-08-23 18:40:27 +0100

    feat: hello world of mjml and uploading

--- scripts/create-mjml-templates-upload-r2.sh
+++ scripts/create-mjml-templates-upload-r2.sh
@@ -1,6 +1,8 @@
 export NO_D1_WARNING=true
 cd src
-# TODO - run script here to generate the latest MJML templates
-find email-templates -name "*.*" | while read fname; do
-    wrangler r2 object put email-templates/$fname -f $fname
+cd email-templates
+# how to do all templates? add a few and tr *
+npx mjml mjml/input.mjml -o output/output.html
+find output -name "*.*" | while read fname; do
+    npx wrangler r2 object put email-templates/$fname -f $fname
 done
