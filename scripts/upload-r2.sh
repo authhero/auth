f54e39a43cbd50033414610cacfdc0b27c26e17f
Daniel Gent <dan@danielgent.com>
2023-08-23 18:36:48 +0100

    fix: seems like wrangler should be NPX

--- scripts/upload-r2.sh
+++ scripts/upload-r2.sh
@@ -1,5 +1,5 @@
 export NO_D1_WARNING=true
 cd src
 find templates -name "*.*" | while read fname; do
-    wrangler r2 object put auth-templates/$fname -f $fname
+    npx wrangler r2 object put auth-templates/$fname -f $fname
 done
