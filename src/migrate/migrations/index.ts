865e8c6c7a58e4774515604dba8e9dda724bdd68
Markus Ahlstrand <markus@ahlstrand.de>
2023-08-28 10:25:30 +0200

    fix: fomat (#70)

--- src/migrate/migrations/index.ts
+++ src/migrate/migrations/index.ts
@@ -2,6 +2,13 @@ import * as m1_init from "./2022-12-11T09:17:35_init";
 import * as m2_domains from "./2023-08-15T09:17:35_domains";
 import * as m3_connection_scopes from "./2023-08-18T14:41:03_connection_scope";
 import * as m4_dkim from "./2023-08-22T15:06:26_dkim";
+import * as m5_styles from "./2023-08-28T09:12:44_styles";
 
 // These needs to be in alphabetic order
-export default { m1_init, m2_domains, m3_connection_scopes, m4_dkim };
+export default {
+  m1_init,
+  m2_domains,
+  m3_connection_scopes,
+  m4_dkim,
+  m5_styles,
+};
