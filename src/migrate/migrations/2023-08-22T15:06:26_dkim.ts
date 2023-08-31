1588750b1d91204bb8ad30d335ffef8c0f7e1014
Markus Ahlstrand <markus@ahlstrand.de>
2023-08-31 22:38:42 +0200

    fix: remove dkim migration

--- src/migrate/migrations/2023-08-22T15:06:26_dkim.ts
+++ src/migrate/migrations/2023-08-22T15:06:26_dkim.ts
@@ -4,15 +4,15 @@ import { Database } from "../../types";
 export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .alterTable("domains")
-    .addColumn("dkimPrivateKey", "varchar(2048)")
-    .addColumn("dkimPublicKey", "varchar(2048)")
+    .addColumn("dkim_private_key", "varchar(2048)")
+    .addColumn("dkim_public_key", "varchar(2048)")
     .execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   await db.schema
     .alterTable("domains")
-    .dropColumn("dkimPrivateKey")
-    .dropColumn("dkimPrivateKey")
+    .dropColumn("dkim_private_key")
+    .dropColumn("dkim_public_key")
     .execute();
 }
