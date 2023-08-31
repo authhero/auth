ee664c2d842cd6963993523b3b5f058fc16461bf
Daniel Gent <dan@danielgent.com>
2023-08-31 13:18:21 +0100

    feat: shortcut script to only upload liquid code template

--- scripts/create-mjml-templates.sh
+++ scripts/create-mjml-templates.sh
@@ -3,3 +3,6 @@ cd src
 
 # hardcode just the swedish code email for now
 npx mjml email-templates/code.mjml -o templates/email/code.liquid
+
+# also upload to wrangler in this script for now so we can quickly upload new email templates
+npx wrangler r2 object put auth-templates/templates/email/code.liquid -f templates/email/code.liquid
