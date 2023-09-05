1521d6c60093ae64ad11c0c12548cb6450e0e168
Daniel Gent <dan@danielgent.com>
2023-09-05 21:58:19 +0100

    fix: fetch tenant id from db

--- src/routes/tsoa/users-mgmt.ts
+++ src/routes/tsoa/users-mgmt.ts
@@ -16,49 +16,26 @@ export class UsersMgmtController extends Controller {
     @Request() request: RequestWithContext,
     @Path("userId") userId: string,
   ): Promise<Profile> {
-    // not actually hitting this! is already 404ing... not getting any log
-    console.log("in getUser");
     const { ctx } = request;
     const { env } = ctx;
 
     const db = getDb(env);
     const dbUser = await db
       .selectFrom("users")
-      // we do not get sent a tenant id
-      // .where("users.tenantId", "=", tenantId)
       .where("users.id", "=", userId)
-      .select("users.email")
+      .selectAll()
       .executeTakeFirst();
 
     if (!dbUser) {
-      console.log("no dbUser");
       throw new NotFoundError();
     }
 
-    // I can find the user by querying the planetscale database
-    // but I need to suffix breakit id for durable object?
-    const tenantId = "JTnV3E4M59SpxiIhhXf6s";
-
-    console.log("dbUser", dbUser);
-    // Fetch the user from durable object
     const user = env.userFactory.getInstanceByName(
-      getId(tenantId, dbUser.email),
-      // userId,
+      getId(dbUser.tenantId, dbUser.email),
     );
-
-    // Invalid Durable Object ID. Durable Object IDs must be 64 hex digits.
-    // const user = env.userFactory.getInstanceById(userId);
-
-    console.log("user", user);
 
     const userResult = user.getProfile.query();
 
-    console.log("userResult", userResult);
-
     return userResult;
-
-    // return "success";
-
-    // return dbUser.email;
   }
 }
