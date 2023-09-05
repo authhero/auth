437e76448dae58808202bcf6565f6bb59d0b4115
Markus Ahlstrand <markus@ahlstrand.de>
2023-09-05 11:15:07 +0200

    feat: add back import script (#103)

--- scripts/import-users.js
+++ scripts/import-users.js
@@ -1,22 +1,61 @@
 const fs = require("fs");
 const readline = require("readline");
 
-async function processLineByLine() {
-  const fileStream = fs.createReadStream("data/sesamy-dev.json");
+const token = "Add token here...";
+const tenantId = "AH_1eG1-Ouam8jRlSd1fI";
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
+async function postUser(user) {
+  const body = JSON.stringify({
+    name: user.name,
+    email: user.email,
+    nickname: user.nickname,
+    picture: user.picture,
+    tags: [],
+    givenName: user.given_name,
+    familyName: user.family_name,
+    id: user.user_id,
+    createdAt: user.created_at,
+    modifiedAt: user.updated_at,
   });
 
-  for await (const line of rl) {
-    // Each line in the input will be successively available here as `line`.
-    // Parse the line as JSON
-    const obj = JSON.parse(line);
+  const response = await fetch(
+    `https://auth2.sesamy.dev/tenants/${tenantId}/users`,
+    {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json",
+      },
+      body,
+    },
+  );
 
-    // Now `obj` is an object, and you can access its properties:
-    console.log(obj);
+  if (!response.ok) {
+    console.log(
+      `Status: ${response.status}, with error ${await response.text()}`,
+    );
   }
 }
 
-processLineByLine();
+async function importUsers(filePath) {
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity, // so it can handle even `\r\n` ending lines correctly
+  });
+
+  let counter = 0;
+
+  for await (const line of rl) {
+    try {
+      const user = JSON.parse(line);
+
+      await postUser(user);
+    } catch (error) {
+      console.error(`Failed to parse line: ${line}. Error: ${error.message}`);
+    }
+  }
+}
+
+importUsers("./data/auth0-dev.json");
