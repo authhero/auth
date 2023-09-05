0c303ad33d4fb87121814841914f8fb8d8985567
Daniel Gent <dan@danielgent.com>
2023-09-05 20:42:01 +0100

    feat: clone existing user route but for api/v2

---
+++ src/routes/tsoa/users-mgmt.ts
@@ -0,0 +1,42 @@
+// not sure what to call this, or where to place it! 8-)
+import { Controller, Get, Request, Route, Tags, Path } from "@tsoa/runtime";
+import { getDb } from "../../services/db";
+import { RequestWithContext } from "../../types/RequestWithContext";
+import { NotFoundError } from "../../errors";
+import { getId } from "../../models";
+import { Profile } from "../../types";
+
+@Route("api/v2/users")
+// @Security("oauth2managementApi", [""])
+@Tags("users-mgmt") // what is tags?
+export class UsersController extends Controller {
+  @Get("")
+  @Get("{userId}")
+  public async getUser(
+    @Request() request: RequestWithContext,
+    @Path("tenantId") tenantId: string,
+    @Path("userId") userId: string,
+  ): Promise<Profile> {
+    const { ctx } = request;
+    const { env } = ctx;
+
+    const db = getDb(env);
+    const dbUser = await db
+      .selectFrom("users")
+      .where("users.tenantId", "=", tenantId)
+      .where("users.id", "=", userId)
+      .select("users.email")
+      .executeTakeFirst();
+
+    if (!dbUser) {
+      throw new NotFoundError();
+    }
+
+    // Fetch the user from durable object
+    const user = env.userFactory.getInstanceByName(
+      getId(tenantId, dbUser.email),
+    );
+
+    return user.getProfile.query();
+  }
+}
