70f0acfcbc4515dbf32b4871895179bd5019386c
Markus Ahlstrand <markus@ahlstrand.de>
2023-09-15 13:20:58 +0200

    feat: tested to detect linked accounts for code

---
+++ src/helpers/account-linking.ts
@@ -0,0 +1,22 @@
+import { Env, Profile } from "../types";
+
+export async function handleLinkedAccount(env: Env, profile: Profile) {
+  if (!profile.linked_with) {
+    return profile;
+  }
+
+  const linkedWith = env.userFactory.getInstanceByName(
+    `${profile.tenant_id}|${profile.linked_with}`,
+  );
+
+  const { connections, ...profileWithoutConnections } = profile;
+
+  return linkedWith.loginWithConnection.mutate({
+    tenantId: profile.tenant_id,
+    email: profile.linked_with,
+    connection: {
+      name: "linked-user",
+      profile: profileWithoutConnections,
+    },
+  });
+}
