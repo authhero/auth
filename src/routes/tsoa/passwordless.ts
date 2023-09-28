efffeb50bc959d3b34654e74f953a77f4e3f5bc9
Daniel Gent <dan@sesamy.com>
2023-09-28 14:47:41 +0200

    fix: of course, need to pull OAuth params from state

--- src/routes/tsoa/passwordless.ts
+++ src/routes/tsoa/passwordless.ts
@@ -165,15 +165,50 @@ export class PasswordlessController extends Controller {
 
       const login2ExpiredCodeUrl = new URL(`${env.LOGIN2_URL}/expired-code`);
 
+      const stateDecoded = new URLSearchParams(state);
+
       login2ExpiredCodeUrl.searchParams.set("email", email);
-      login2ExpiredCodeUrl.searchParams.set("client_id", client_id);
-      login2ExpiredCodeUrl.searchParams.set("connection", "auth2");
-      // is this the correct state? release and find out?
-      login2ExpiredCodeUrl.searchParams.set("state", state);
-      login2ExpiredCodeUrl.searchParams.set("redirect_uri", redirect_uri);
-      login2ExpiredCodeUrl.searchParams.set("response_type", response_type);
-      login2ExpiredCodeUrl.searchParams.set("scope", scope);
-      // any other params? nonce, audience - let's release and see!
+
+      const redirectUri = stateDecoded.get("redirect_uri");
+      if (redirectUri) {
+        login2ExpiredCodeUrl.searchParams.set("redirect_uri", redirectUri);
+      }
+
+      const audience = stateDecoded.get("audience");
+      if (audience) {
+        login2ExpiredCodeUrl.searchParams.set("audience", audience);
+      }
+
+      const nonce = stateDecoded.get("nonce");
+      if (nonce) {
+        login2ExpiredCodeUrl.searchParams.set("nonce", nonce);
+      }
+
+      const scope = stateDecoded.get("scope");
+      if (scope) {
+        login2ExpiredCodeUrl.searchParams.set("scope", scope);
+      }
+
+      const responseType = stateDecoded.get("response_type");
+      if (responseType) {
+        login2ExpiredCodeUrl.searchParams.set("response_type", responseType);
+      }
+
+      const state2 = stateDecoded.get("state");
+      if (state2) {
+        login2ExpiredCodeUrl.searchParams.set("state", state2);
+      }
+
+      const client_id = stateDecoded.get("client_id");
+      if (client_id) {
+        login2ExpiredCodeUrl.searchParams.set("client_id", client_id);
+      }
+
+      // this will always be auth2
+      const connection2 = stateDecoded.get("connection");
+      if (connection2) {
+        login2ExpiredCodeUrl.searchParams.set("connection", connection2);
+      }
 
       this.setHeader(headers.location, login2ExpiredCodeUrl.toString());
 
