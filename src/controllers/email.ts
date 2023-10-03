80b8a55f467b9fd56d8c900c42f60fba57c6e407
Daniel Gent <dan@sesamy.com>
2023-10-03 22:31:27 +0200

    feat: new template for code emails and send this depending on call to passwordless/start (#175)
    * feat: existing code email should be link template
    
    * feat: remove magic link from code template. new copy
    
    * feat: duplicate sendcode logic so also sendlink with new template
    
    * feat: add link template to upload scripts
    
    * test: fix types on tests

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -63,6 +63,54 @@ export async function sendCode(
   client: Client,
   to: string,
   code: string,
+) {
+  const response = await env.AUTH_TEMPLATES.get("templates/email/link.liquid");
+  if (!response) {
+    throw new Error("Code template not found");
+  }
+
+  const templateString = await response.text();
+
+  const language = client.tenant.language || "sv";
+
+  const logo = getClientLogoPngGreyBg(
+    client.tenant.logo ||
+      "https://assets.sesamy.com/static/images/sesamy/logo-translucent.png",
+    env.IMAGE_PROXY_URL,
+  );
+
+  const sendCodeTemplate = engine.parse(templateString);
+  const codeEmailBody = await engine.render(sendCodeTemplate, {
+    code,
+    vendorName: client.name,
+    logo,
+    supportUrl: client.tenant.support_url || "https://support.sesamy.com",
+    buttonColor: client.tenant.primary_color || "#7d68f4",
+  });
+
+  await sendEmail(client, {
+    to: [{ email: to, name: to }],
+    from: {
+      email: client.tenant.sender_email,
+      name: client.tenant.sender_name,
+    },
+    content: [
+      {
+        type: "text/html",
+        value: codeEmailBody,
+      },
+    ],
+    subject: translate(language, "codeEmailTitle")
+      .replace("{{vendorName}}", client.name)
+      .replace("{{code}}", code),
+  });
+}
+
+export async function sendLink(
+  env: Env,
+  client: Client,
+  to: string,
+  code: string,
   magicLink: string,
 ) {
   const response = await env.AUTH_TEMPLATES.get("templates/email/code.liquid");
