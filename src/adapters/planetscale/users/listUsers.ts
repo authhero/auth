a19d8d2b813ac109ba73651a02dd3a5292f96714
Daniel Gent <dan@sesamy.com>
2023-10-25 13:34:51 +0200

    fix: types on planetscale list users

--- src/adapters/planetscale/users/listUsers.ts
+++ src/adapters/planetscale/users/listUsers.ts
@@ -10,11 +10,18 @@ function getCountAsInt(count: string | number | bigint) {
     return parseInt(count, 10);
   }
 
+  if (typeof count === "bigint") {
+    return Number(count);
+  }
+
   return count;
 }
 
 export function listUsers(db: Kysely<Database>) {
-  return async (tenantId, params: ListUserParams) => {
+  return async (
+    tenantId,
+    params: ListUserParams,
+  ): Promise<{ data: UserResponse[]; totals?: Totals }> => {
     let query = db.selectFrom("users").where("users.tenant_id", "=", tenantId);
 
     if (params.sort && params.sort.sort_by) {
@@ -32,9 +39,39 @@ export function listUsers(db: Kysely<Database>) {
 
     const users = await filteredQuery.selectAll().execute();
 
+    const data: UserResponse[] = users.map((user) => {
+      const { id, modified_at, ...userWithoutFields } = user;
+
+      const tags = JSON.parse(user.tags || "[]");
+
+      const userResponse: UserResponse = {
+        user_id: user.id,
+        // TODO: store this field in sql
+        email_verified: true,
+        username: user.email,
+        phone_number: "",
+        phone_verified: false,
+        updated_at: modified_at,
+        logins_count: 0,
+        identities: tags.map((tag) => ({
+          profileData: {
+            email: user.email,
+            user_id: user.id,
+            is_social: true,
+            connection: tag,
+          },
+        })),
+        ...userWithoutFields,
+        // Deprecated
+        tags,
+      };
+
+      return userResponse;
+    });
+
     if (!params.include_totals) {
       return {
-        users,
+        data,
       };
     }
 
@@ -45,10 +82,12 @@ export function listUsers(db: Kysely<Database>) {
     const countInt = getCountAsInt(count);
 
     return {
-      data: users,
-      start: (params.page - 1) * params.per_page,
-      limit: params.per_page,
-      length: countInt,
+      data,
+      totals: {
+        start: (params.page - 1) * params.per_page,
+        limit: params.per_page,
+        length: countInt,
+      },
     };
   };
 }
