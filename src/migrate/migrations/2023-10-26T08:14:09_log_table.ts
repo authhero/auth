e88928c6a3ac3b4960977185db01bb0155c6dad7
Daniel Gent <dan@sesamy.com>
2023-10-26 10:25:59 +0200

    feat: first attempt at logs route

--- src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
+++ src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
@@ -5,8 +5,11 @@ export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .createTable("logs")
     .addColumn("id", "varchar(255)", (col) => col.notNull().primaryKey())
-    .addColumn("client_id", "varchar(255)", (col) =>
-      col.references("clients.id").onDelete("cascade").notNull(),
+    .addColumn("tenant_id", "varchar(255)", (col) =>
+      col.references("tenants.id").onDelete("cascade").notNull(),
+    )
+    .addColumn("user_id", "varchar(255)", (col) =>
+      col.references("users.id").onDelete("cascade").notNull(),
     )
     .addColumn("timestamp", "varchar(255)", (col) => col.notNull())
     .addColumn("category", "varchar(255)", (col) => col.notNull())
--- src/routes/tsoa/logs.ts
+++ src/routes/tsoa/logs.ts
@@ -8,10 +8,7 @@ import {
   Security,
   Tags,
 } from "@tsoa/runtime";
-import { nanoid } from "nanoid";
-import { NotFoundError } from "../../errors";
 import { getDb } from "../../services/db";
-import { getId } from "../../models";
 import { RequestWithContext } from "../../types/RequestWithContext";
 import { z } from "zod";
 import { headers } from "../../constants";
@@ -21,43 +18,16 @@ export const LogsFilterSchema = z.object({
   userId: z.string(),
 });
 
-function applySort(logs: LogMessage[], sort?: string) {
-  if (!sort) {
-    return logs;
-  }
-
-  const [column, order] = JSON.parse(sort);
-
-  logs.sort((a, b) =>
-    order === "DESC"
-      ? b[column]?.localeCompare(a[column])
-      : a[column]?.localeCompare(b[column]),
-  );
-
-  return logs;
-}
-
 @Route("tenants/{tenantId}/logs")
 @Tags("logs")
 @Security("oauth2managementApi", [""])
 export class LogsController extends Controller {
-  applyRange(logs: LogMessage[], range?: string) {
-    if (!range) {
-      return logs;
-    }
-
-    this.setHeader(headers.contentRange, `logs=${0}-${10}/${logs.length}`);
-
-    const [from, to] = JSON.parse(range);
-
-    return logs.slice(from, to);
-  }
-
   @Get("")
   public async getUserLogs(
     @Request() request: RequestWithContext,
     @Path("tenantId") tenantId: string,
     @Query("filter") filterJson: string,
+    // TODO - implement these in SQL
     @Query("range") range?: string,
     @Query("sort") sort?: string,
   ) {
@@ -67,28 +37,16 @@ export class LogsController extends Controller {
     const filter = LogsFilterSchema.parse(JSON.parse(filterJson));
 
     const db = getDb(env);
-    const dbUser = await db
-      .selectFrom("users")
-      .where("users.tenant_id", "=", tenantId)
-      .where("users.id", "=", filter.userId)
-      .select("users.email")
+    const logs = await db
+      .selectFrom("logs")
+      .where("logs.tenant_id", "=", tenantId)
+      .where("logs.user_id", "=", filter.userId)
+      .selectAll()
       .executeTakeFirst();
 
-    if (!dbUser) {
-      throw new NotFoundError();
-    }
+    // need to also do this? check what code has changed...
+    // this.setHeader(headers.contentRange, `logs=${0}-${10}/${logs.length}`);
 
-    // Fetch the user from durable object
-    const user = env.userFactory.getInstanceByName(
-      getId(tenantId, dbUser.email),
-    );
-
-    const logs = await user.getLogs.query();
-
-    return this.applyRange(applySort(logs, sort), range).map((log) => ({
-      ...log,
-      // TODO: this is a temporary hack as we didn't have id in the start. Can be removed once the users are recreated.
-      id: nanoid(),
-    }));
+    return logs;
   }
 }
