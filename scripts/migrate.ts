bf56616ea8b19f391f771e2406a239d5433ca0e7
Markus Ahlstrand <markus@ahlstrand.de>
2023-10-26 14:22:56 +0200

    fix: added down script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -51,6 +51,29 @@ async function migrateToLatest() {
   await db.destroy();
 }
 
+async function migrateDown() {
+  const provider = new ReferenceMigrationProvider(migrations);
+  const db = getDb(dialect);
+  const migrator = new Migrator({
+    db,
+    provider,
+  });
+  const { error, results } = await migrator.migrateDown();
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was reverted successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+  if (error) {
+    console.error("failed to migrate");
+    console.error(error);
+    throw error;
+  }
+  await db.destroy();
+}
+
 migrateToLatest()
   .then(() => {
     console.log("done");
