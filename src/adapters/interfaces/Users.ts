c43f1670a85cc98de75bbd0adc480e1e35da90f0
Daniel Gent <dan@sesamy.com>
2023-10-26 11:53:47 +0200

    fix: superflous ListUserParams

--- src/adapters/interfaces/Users.ts
+++ src/adapters/interfaces/Users.ts
@@ -1,16 +1,6 @@
 import { Totals } from "../../types/auth0/Totals";
 import { PostUsersBody, UserResponse } from "../../types/auth0/UserResponse";
-
-export interface ListUserParams {
-  page: number;
-  per_page: number;
-  include_totals: boolean;
-  q?: string;
-  sort?: {
-    sort_by: string;
-    sort_order: "asc" | "desc";
-  };
-}
+import { ListParams } from "./ListParams";
 
 export interface ListUsersResponse extends Totals {
   users: UserResponse[];
@@ -18,5 +8,5 @@ export interface ListUsersResponse extends Totals {
 
 export interface UserDataAdapter {
   create(tenantId: string, PostUsersBody): Promise<UserResponse>;
-  list(tenantId: string, params: ListUserParams): Promise<ListUsersResponse>;
+  list(tenantId: string, params: ListParams): Promise<ListUsersResponse>;
 }
