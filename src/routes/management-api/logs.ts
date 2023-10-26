6ff43187079e63e6f8a385af1807592e86e584f6
Daniel Gent <dan@sesamy.com>
2023-10-26 18:58:30 +0200

    fix: logs route to be similar to users

--- src/routes/management-api/logs.ts
+++ src/routes/management-api/logs.ts
@@ -8,10 +8,9 @@ import {
   Security,
   Tags,
 } from "@tsoa/runtime";
-import { getDb } from "../../services/db";
 import { RequestWithContext } from "../../types/RequestWithContext";
 import { z } from "zod";
-import { headers } from "../../constants";
+import { ListLogsResponse } from "../../adapters/interfaces/Logs";
 
 export const LogsFilterSchema = z.object({
   userId: z.string(),
@@ -25,28 +24,33 @@ export class LogsController extends Controller {
   public async getUserLogs(
     @Request() request: RequestWithContext,
     @Header("tenant-id") tenantId: string,
-    @Query("filter") filterJson: string,
-    // TODO - implement these in SQL
-    @Query("range") range?: string,
-    @Query("sort") sort?: string,
+    // Auth0
+    @Query() page = 0,
+    @Query() per_page = 20,
+    @Query() include_totals = false,
+    @Query() sort?: string,
+    @Query() connection?: string,
+    @Query() fields?: string,
+    @Query() include_fields?: boolean,
+    @Query() q?: string,
   ) {
     const { ctx } = request;
     const { env } = ctx;
 
-    const filter = LogsFilterSchema.parse(JSON.parse(filterJson));
+    const result = await env.data.logs.list(tenantId, {
+      page,
+      per_page,
+      include_totals,
+      // TODO - sorting!
+      // sort: parseSort(sort),
+      q,
+    });
 
-    const db = getDb(env);
-    // TODO - this needs to be an adaptor of course! so we can support in-memory
-    const logs = await db
-      .selectFrom("logs")
-      .where("logs.tenant_id", "=", tenantId)
-      .where("logs.user_id", "=", filter.userId)
-      .selectAll()
-      .executeTakeFirst();
+    if (include_totals) {
+      // don't like this type casting. copied from users route...
+      return result as ListLogsResponse;
+    }
 
-    // need to also do this? check what code has changed...
-    // this.setHeader(headers.contentRange, `logs=${0}-${10}/${logs.length}`);
-
-    return logs;
+    return result.logs;
   }
 }
