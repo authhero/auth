af40fc8782e3b2f58f964ced0829ffca7fdffe5d
Daniel Gent <dan@sesamy.com>
2023-10-26 10:21:02 +0200

    feat: client_id FK in logs table

--- src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
+++ src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
@@ -5,6 +5,9 @@ export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .createTable("logs")
     .addColumn("id", "varchar(255)", (col) => col.notNull().primaryKey())
+    .addColumn("client_id", "varchar(255)", (col) =>
+      col.references("clients.id").onDelete("cascade").notNull(),
+    )
     .addColumn("timestamp", "varchar(255)", (col) => col.notNull())
     .addColumn("category", "varchar(255)", (col) => col.notNull())
     .addColumn("message", "varchar(255)", (col) => col.notNull());
