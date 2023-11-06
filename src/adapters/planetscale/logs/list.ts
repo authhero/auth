df2482a7e8b22466ac4bdab11e7a69b0a6f75a99
Daniel Gent <dan@sesamy.com>
2023-11-06 18:05:17 +0100

    fix: now working

--- src/adapters/planetscale/logs/list.ts
+++ src/adapters/planetscale/logs/list.ts
@@ -4,26 +4,24 @@ import { ListParams } from "../../../adapters/interfaces/ListParams";
 
 export function listLogs(db: Kysely<Database>) {
   return async (tenantId, params: ListParams) => {
+    console.log("params", params);
+
     if (!params.q) {
       throw new Error("No user_id provided");
     }
 
-    const userId = params.q.split("user_id:")[1];
-
-    console.log("userId ", userId);
+    const userId = decodeURIComponent(params.q).split("user_id:")[1];
 
     if (!userId) {
       throw new Error("No user_id provided");
     }
 
+    console.log("userId", userId);
+
     let query = db
       .selectFrom("logs")
       .where("logs.tenant_id", "=", tenantId)
       .where("logs.user_id", "=", userId);
-
-    // user_id
-
-    console.log("params: ", params);
 
     // this kept blowing up for me but we could reinstate it...
     // if (params.q) {
@@ -32,14 +30,16 @@ export function listLogs(db: Kysely<Database>) {
     //   );
     // }
 
+    // TODO - implement this
     // if (params.sort && params.sort.sort_by) {
     //   const { ref } = db.dynamic;
     //   query = query.orderBy(ref(params.sort.sort_by), params.sort.sort_order);
     // }
 
-    const filteredQuery = query;
-    // .offset((params.page - 1) * params.per_page)
-    // .limit(params.per_page);
+    const filteredQuery = query
+      // TODO - keep getting odd errors in SQL syntax
+      .offset((params.page - 1) * params.per_page)
+      .limit(params.per_page);
 
     const logs = await filteredQuery.selectAll().execute();
 
