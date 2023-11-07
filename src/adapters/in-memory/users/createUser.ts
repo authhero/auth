162120b59f12d9736cfc0a7c437a807732fe7988
Daniel Gent <dan@sesamy.com>
2023-11-07 10:03:13 +0100

    fix: in-memory user type

--- src/adapters/in-memory/users/createUser.ts
+++ src/adapters/in-memory/users/createUser.ts
@@ -1,9 +1,20 @@
 import { SqlUser } from "../../../types";
+import { PostUsersBody, UserResponse } from "../../../types/auth0/UserResponse";
+import { nanoid } from "nanoid";
 
 export function createUser(users: SqlUser[]) {
-  return async (tenantId: string, user: SqlUser): Promise<SqlUser> => {
-    users.push(user);
+  return async (tenantId: string, user: PostUsersBody): Promise<SqlUser> => {
+    const sqlUser: SqlUser = {
+      ...user,
+      email: user.email || "",
+      tenant_id: tenantId,
+      id: nanoid(),
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    };
 
-    return user;
+    users.push(sqlUser);
+
+    return sqlUser;
   };
 }
