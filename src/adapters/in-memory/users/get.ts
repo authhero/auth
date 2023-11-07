9d803fc345fd2ae07c5b87c19230f390b1a7c249
Markus Ahlstrand <markus@ahlstrand.de>
2023-11-07 06:45:04 +0100

    Update src/adapters/in-memory/users/get.ts
    Co-authored-by: Daniel Gent <dan@sesamy.com>

--- src/adapters/in-memory/users/get.ts
+++ src/adapters/in-memory/users/get.ts
@@ -2,6 +2,6 @@ import { SqlUser } from "../../../types";
 
 export function get(users: SqlUser[]) {
   return async (tenant_id: string, id: string): Promise<SqlUser | null> => {
-    return users.find((user) => user.id === id) || null;
+    return users.find((user) => user.id === id && user.tenant_id === tenant_id) || null;
   };
 }
