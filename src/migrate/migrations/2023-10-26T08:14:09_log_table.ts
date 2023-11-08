5bc16d3920871722b89dcfc34bce321b334ba62b
Daniel Gent <dan@sesamy.com>
2023-11-08 17:15:48 +0100

    fix: revert previous logs table migration to as it was on prod

--- src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
+++ src/migrate/migrations/2023-10-26T08:14:09_log_table.ts
@@ -5,11 +5,8 @@ export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .createTable("logs")
     .addColumn("id", "varchar(255)", (col) => col.notNull().primaryKey())
-    .addColumn("tenant_id", "varchar(255)", (col) =>
-      col.references("tenants.id").onDelete("cascade").notNull(),
-    )
-    .addColumn("user_id", "varchar(255)", (col) =>
-      col.references("users.id").onDelete("cascade").notNull(),
+    .addColumn("client_id", "varchar(255)", (col) =>
+      col.references("clients.id").onDelete("cascade").notNull(),
     )
     .addColumn("timestamp", "varchar(255)", (col) => col.notNull())
     .addColumn("category", "varchar(255)", (col) => col.notNull())
