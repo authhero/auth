349d601013945e4f4d4c0c157f61b44267ab8484
Daniel Gent <dan@sesamy.com>
2023-11-09 10:45:57 +0100

    fix: user adapter uses sqluser everywhere

--- src/adapters/interfaces/Users.ts
+++ src/adapters/interfaces/Users.ts
@@ -1,15 +1,17 @@
 import { SqlUser } from "../../types";
 import { Totals } from "../../types/auth0/Totals";
-import { PostUsersBody, UserResponse } from "../../types/auth0/UserResponse";
+// also PostUsersBody... should this be handled in the route? hmmmmmmm. Yes right?
+// so then can revert the tests to use SqlUser... easier fixtures
+import { PostUsersBody } from "../../types/auth0/UserResponse";
 import { ListParams } from "./ListParams";
 
 export interface ListUsersResponse extends Totals {
-  users: UserResponse[];
+  users: SqlUser[];
 }
 
 export interface UserDataAdapter {
-  get(tenant_id: string, id: string): Promise<UserResponse | null>;
-  getByEmail(tenant_id: string, email: string): Promise<UserResponse | null>;
-  create(tenantId: string, PostUsersBody): Promise<UserResponse>;
+  get(tenant_id: string, id: string): Promise<SqlUser | null>;
+  getByEmail(tenant_id: string, email: string): Promise<SqlUser | null>;
+  create(tenantId: string, PostUsersBody): Promise<SqlUser>;
   list(tenantId: string, params: ListParams): Promise<ListUsersResponse>;
 }
