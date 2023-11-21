bb679a53e52fdb339cdc874d5c657c602afbd22a
Markus Ahlstrand <markus@ahlstrand.de>
2023-11-21 15:34:59 +0100

    fix: review comment

--- src/adapters/planetscale/users/create.ts
+++ src/adapters/planetscale/users/create.ts
@@ -1,25 +1,10 @@
 import { Kysely } from "kysely";
-import { nanoid } from "nanoid";
 import { Database, SqlUser, User } from "../../../types";
 
 export function create(db: Kysely<Database>) {
   return async (tenantId: string, user: User): Promise<SqlUser> => {
-    const user_id = user.id || nanoid();
     const sqlUser: SqlUser = {
-      id: user_id,
-      email: user.email || "",
-      given_name: user.given_name,
-      family_name: user.family_name,
-      name: user.name,
-      nickname: user.nickname,
-      picture: user.picture,
-      email_verified: user.email_verified,
-      last_ip: user.last_ip,
-      last_login: user.last_login,
-      login_count: user.login_count,
-      provider: user.provider,
-      connection: user.connection,
-      is_social: user.is_social,
+      ...user,
       created_at: new Date().toISOString(),
       updated_at: new Date().toISOString(),
       tenant_id: tenantId,
