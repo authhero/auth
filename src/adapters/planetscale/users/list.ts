f3bdcf953107a6962f4b2e1fc36834b8edd8c633
Daniel Gent <dan@sesamy.com>
2023-11-24 17:56:26 +0100

    fix: extract out common util for casting count to int

--- src/adapters/planetscale/users/list.ts
+++ src/adapters/planetscale/users/list.ts
@@ -2,20 +2,7 @@ import { ListUsersResponse } from "../../interfaces/Users";
 import { Database } from "../../../types";
 import { Kysely, SelectQueryBuilder } from "kysely";
 import { ListParams } from "../../interfaces/ListParams";
-
-// duplicated from tenants
-function getCountAsInt(count: string | number | bigint) {
-  // VScode complains that parseInt only accepts a string BUT the project builds & lints
-  if (typeof count === "string") {
-    return parseInt(count, 10);
-  }
-
-  if (typeof count === "bigint") {
-    return Number(count);
-  }
-
-  return count;
-}
+import getCountAsInt from "../../../utils/getCountAsInt";
 
 const searchableColumns = ["email", "name"];
 
