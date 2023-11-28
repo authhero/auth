11948039d6ebc10eed6c34d5f42a3d1453f44998
Daniel Gent <dan@sesamy.com>
2023-11-28 10:28:06 +0100

    feat: first attempt at encode/decode state function and tests

---
+++ src/utils/stateEncode.ts
@@ -0,0 +1,15 @@
+// this util function might not be necessary BUT we've got so many util functions
+// I worry we might need to chain a few up
+
+// typescript key value object
+
+// are values always string? no they can also be objects...
+type stateObject = { [key: string]: any };
+
+export function stateEncode(state: stateObject) {
+  return atob(JSON.stringify(state));
+}
+
+export function stateDecode(state: string) {
+  return JSON.parse(btoa(state));
+}
---
+++ test/utils/stateEncode.spec.ts
@@ -0,0 +1,32 @@
+import { stateEncode, stateDecode } from "../../src/utils/stateEncode";
+
+describe("stateEncode", () => {
+  it("should encode a state object to a string", () => {
+    const state = {
+      userId: "123",
+      authParams: "abc",
+      nonce: "xyz",
+      state: "state",
+      sid: "sid",
+      user: "user",
+    };
+    expect(stateEncode(state)).toEqual(
+      "eyJ1c2VySWQiOiIxMjMiLCJhdXRoUGFyYW1zIjoiYWJjIiwibm9uY2UiOiJ4eXoiLCJzdGF0ZSI6InN0YXRlIiwic2lkIjoic2lkIiwidXNlciI6InVzZXIifQ==",
+    );
+  });
+});
+
+describe("stateDecode", () => {
+  it("should decode a state string to an object", () => {
+    const state =
+      "eyJ1c2VySWQiOiIxMjMiLCJhdXRoUGFyYW1zIjoiYWJjIiwibm9uY2UiOiJ4eXoiLCJzdGF0ZSI6InN0YXRlIiwic2lkIjoic2lkIiwidXNlciI6InVzZXIifQ==";
+    expect(stateDecode(state)).toEqual({
+      userId: "123",
+      authParams: "abc",
+      nonce: "xyz",
+      state: "state",
+      sid: "sid",
+      user: "user",
+    });
+  });
+});
