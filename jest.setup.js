02b5e1d5d0b006c7f92777b9fdcb072e70c4ccae
Daniel Gent <dan@sesamy.com>
2023-11-28 13:01:33 +0100

    test: fix time

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,7 @@
 import { TextEncoder, TextDecoder } from "util";
 import { subtle } from "node:crypto";
 import fetch from "jest-fetch-mock";
+import mockdate from "mockdate";
 
 global.crypto.subtle = subtle;
 
@@ -9,3 +10,9 @@ global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
 global.fetch = fetch;
+
+mockdate.set("2023-11-28T12:00:00Z");
+
+afterAll(() => {
+  mockdate.reset();
+});
