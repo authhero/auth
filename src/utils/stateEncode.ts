541cbf78bab2feb96057423e5f3bb273e785c33a
Daniel Gent <dan@sesamy.com>
2023-11-29 14:56:31 +0100

    fix: use url safe base64 methods

--- src/utils/stateEncode.ts
+++ src/utils/stateEncode.ts
@@ -1,9 +1,10 @@
 type stateObject = { [key: string]: any };
+import { base64UrlEncode, base64UrlDecode } from "./base64";
 
 export function stateEncode(state: stateObject) {
-  return btoa(JSON.stringify(state));
+  return base64UrlEncode(JSON.stringify(state));
 }
 
 export function stateDecode(state: string) {
-  return JSON.parse(atob(state));
+  return JSON.parse(base64UrlDecode(state));
 }
