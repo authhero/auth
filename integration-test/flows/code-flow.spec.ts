a5310cb6a6d0997f85abd4701b5a3d602f5d8308
Daniel Gent <dan@sesamy.com>
2023-12-04 21:34:45 +0100

    fix: decode silent auth id token

--- integration-test/flows/code-flow.spec.ts
+++ integration-test/flows/code-flow.spec.ts
@@ -115,9 +115,8 @@ describe("code-flow", () => {
     expect(idTokenPayload.email).toBe("test@example.com");
     expect(idTokenPayload.aud).toBe("clientId");
 
-    // now check silent auth works
-    console.log("COOKIES ", tokenResponse.headers.get("set-cookie"));
-
+    // now check silent auth works when logged in with code----------------------------------------
+    // TODO - duplicate this test to the end of other flows
     const setCookiesHeader = tokenResponse.headers.get("set-cookie");
     const cookies = setCookiesHeader.split(";").map((c) => c.trim());
     const authCookie = cookies.find((c) => c.startsWith("auth-token"));
@@ -142,6 +141,7 @@ describe("code-flow", () => {
       `/authorize?${silentAuthSearchParams.toString()}`,
       {
         headers: {
+          // here we set the auth cookie given to us from the previous successful auth request
           cookie: authCookie,
         },
       },
@@ -152,11 +152,47 @@ describe("code-flow", () => {
     expect(body).not.toContain("Login required");
 
     expect(body).toContain("access_token");
-    expect(body).toContain("id_token");
 
-    // could also parse these tokens here and sanity check!
+    // get id token from iframe response body
+    const lines = body.split("\n");
+    const responseBody = lines.find((line) =>
+      line.trim().startsWith("response: "),
+    );
+    if (!responseBody) {
+      throw new Error("iframe auth body missing");
+    }
 
-    // and also test for different client_ids that have the same tenant_id
-    // and also different client_ids with DIFFERENT tenant_ids
+    const iframeResponseJSON = JSON.parse(
+      responseBody.replace("response: ", ""),
+    );
+
+    const silentAuthIdToken = iframeResponseJSON.id_token;
+
+    const silentAuthIdTokenPayload = parseJwt(silentAuthIdToken);
+
+    const {
+      // these are the fields that change on every test run
+      exp,
+      iat,
+      sid,
+      sub,
+      ...restOfIdTokenPayload
+    } = silentAuthIdTokenPayload;
+
+    expect(sub).toContain("email|");
+    expect(sid).toHaveLength(21);
+    expect(restOfIdTokenPayload).toEqual({
+      aud: "clientId",
+      name: "test@example.com",
+      email: "test@example.com",
+      email_verified: true,
+      nonce: "ehiIoMV7yJCNbSEpRq513IQgSX7XvvBM",
+      iss: "https://example.com/",
+    });
   });
+
+  // FUTURE TESTS
+  // and also test for different client_ids that have the same tenant_id
+  // ---- this is what I wrote a test for on login2 - may as well have quicker integration test here...
+  // can also test for clients with different tenants - will stop the regressions we keep getting
 });
