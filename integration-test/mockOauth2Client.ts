35b3634b7f08b264d3528e846b340901a01ef98b
Daniel Gent <dan@sesamy.com>
2023-12-04 23:51:41 +0100

    test: another mock id_token

--- integration-test/mockOauth2Client.ts
+++ integration-test/mockOauth2Client.ts
@@ -20,8 +20,10 @@ class MockOAuth2Client implements IOAuth2Client {
     if (this.params.client_id === "otherSocialClientId") {
       return {
         access_token: "otherClientAccessToken",
-        // we're actually not checking anything from the id_token... the client_id is encoded in the state
-        id_token: "??????",
+        // TODO - create a new id_token - same but with different sub
+        id_token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL29wdGhlci1hdXRoLmV4YW1wbGUuY29tIiwic3ViIjoidGVzdC1uZXctc3ViIiwiYXVkIjoiY2xpZW50MTIzIiwiZXhwIjoxNjE2NDcwOTQ4LCJpYXQiOjE2MTY0NjczNDgsIm5hbWUiOiJKb2huIERvZSIsImVtYWlsIjoiam9obi5kb2UuZXhhbXBsZS5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9qb2huLmpwZyIsIm5vbmNlIjoiYWJjMTIzIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9.7GhGwUagcim1sIHyw-3ZrxOYyS2Nccq8vNc1nH-KR3M",
+
         token_type: "tokenType",
         expires_in: 1000,
         refresh_token: "refreshToken",
