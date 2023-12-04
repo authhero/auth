fe74e00b073e064d8a7d7ccb9db7d977c9646cd0
Daniel Gent <dan@sesamy.com>
2023-12-04 21:50:02 +0100

    test: silent auth test on magic link flow

--- integration-test/flows/magic-link-flow.spec.ts
+++ integration-test/flows/magic-link-flow.spec.ts
@@ -95,5 +95,81 @@ describe("code-flow", () => {
     const idTokenPayload = parseJwt(idToken!);
     expect(idTokenPayload.email).toBe("test@example.com");
     expect(idTokenPayload.aud).toBe("clientId");
+
+    // now check silent auth works when logged in with code----------------------------------------
+    const cookies = autenticateResponse.headers
+      .get("set-cookie")
+      .split(";")
+      .map((c) => c.trim());
+    const authCookie = cookies.find((c) => c.startsWith("auth-token"));
+
+    expect(authCookie).toBeDefined();
+
+    const silentAuthSearchParams = new URLSearchParams();
+    silentAuthSearchParams.set("client_id", "clientId");
+    silentAuthSearchParams.set("response_type", "token id_token");
+    silentAuthSearchParams.set("scope", "openid profile email");
+    silentAuthSearchParams.set(
+      "redirect_uri",
+      "http://localhost:3000/callback",
+    );
+    silentAuthSearchParams.set("state", "state");
+    // silent auth pararms!
+    silentAuthSearchParams.set("prompt", "none");
+    silentAuthSearchParams.set("nonce", nonce);
+    silentAuthSearchParams.set("response_mode", "web_message");
+
+    const silentAuthResponse = await worker.fetch(
+      `/authorize?${silentAuthSearchParams.toString()}`,
+      {
+        headers: {
+          // here we set the auth cookie given to us from the previous successful auth request
+          cookie: authCookie,
+        },
+      },
+    );
+
+    const body = await silentAuthResponse.text();
+
+    expect(body).not.toContain("Login required");
+
+    expect(body).toContain("access_token");
+
+    // get id token from iframe response body
+    const lines = body.split("\n");
+    const responseBody = lines.find((line) =>
+      line.trim().startsWith("response: "),
+    );
+    if (!responseBody) {
+      throw new Error("iframe auth body missing");
+    }
+
+    const iframeResponseJSON = JSON.parse(
+      responseBody.replace("response: ", ""),
+    );
+
+    const silentAuthIdToken = iframeResponseJSON.id_token;
+
+    const silentAuthIdTokenPayload = parseJwt(silentAuthIdToken);
+
+    const {
+      // these are the fields that change on every test run
+      exp,
+      iat,
+      sid,
+      sub,
+      ...restOfIdTokenPayload
+    } = silentAuthIdTokenPayload;
+
+    expect(sub).toContain("email|");
+    expect(sid).toHaveLength(21);
+    expect(restOfIdTokenPayload).toEqual({
+      aud: "clientId",
+      name: "test@example.com",
+      email: "test@example.com",
+      email_verified: true,
+      nonce: "ehiIoMV7yJCNbSEpRq513IQgSX7XvvBM",
+      iss: "https://example.com/",
+    });
   });
 });
