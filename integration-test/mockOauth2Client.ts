dd8c830afc00476c1e52e5b10500d234a95756d6
Daniel Gent <dan@sesamy.com>
2023-12-04 19:05:59 +0100

    fix: remove comments

--- integration-test/mockOauth2Client.ts
+++ integration-test/mockOauth2Client.ts
@@ -20,7 +20,6 @@ class MockOAuth2Client implements IOAuth2Client {
     return new Promise((resolve, reject) => {
       resolve({
         access_token: "accessToken",
-        // copy-pasted from chat GPT into jwt.io - should create programmatically here?
         id_token:
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2F1dGguZXhhbXBsZS5jb20iLCJzdWIiOiIxMjM0NTY3ODkwIiwiYXVkIjoiY2xpZW50MTIzIiwiZXhwIjoxNjE2NDcwOTQ4LCJpYXQiOjE2MTY0NjczNDgsIm5hbWUiOiJKb2huIERvZSIsImVtYWlsIjoiam9obi5kb2VAZXhhbXBsZS5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9qb2huLmpwZyIsIm5vbmNlIjoiYWJjMTIzIn0.yw00sUxyb4FnkJHu05Q_uPgP9c63izvrNjvYissY-kY",
         token_type: "tokenType",
