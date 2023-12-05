2f09f274ca14444eaa69621d74b9f576c5a5b33a
Daniel Gent <dan@sesamy.com>
2023-12-05 18:27:52 +0100

    fix: planetscale delete tenant UserDataAdapter

---
+++ src/adapters/kysely/tenants/removeTenant.ts
@@ -0,0 +1,13 @@
+import { Kysely } from "kysely";
+import { Database } from "../../../types";
+
+export function removeTenant(db: Kysely<Database>) {
+  return async (tenant_id: string): Promise<boolean> => {
+    const results = await db
+      .deleteFrom("tenants")
+      .where("tenants.id", "=", tenant_id)
+      .execute();
+
+    return results.length === 1;
+  };
+}
