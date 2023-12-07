641639fd7823536d5f4f3e72d7eeccadff5d3a2a
Daniel Gent <dan@sesamy.com>
2023-12-07 17:27:25 +0100

    feat: link email vars extracted

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,4 +2,5 @@ export const en = {
   codeEmailTitle: "Welcome to {{vendorName}}! {{code}} is the login code",
   codeEmailWelcometoYourAccount: "Welcome to your {{vendorName}} account!",
   passwordResetTitle: "Change password for your {{vendorName}} account",
+  // TODO - insert translations here!
 };
--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -1,13 +1,21 @@
 export const sv = {
+  // common
+  welcomeToYourAccount: "Välkommen till ditt {{vendorName}} konto!",
+  contactUs: "Kontakta oss",
+  copyright: "Copyright © 2023 SESAMY. Alla rättigheter förbehållna.",
+  // code email
   codeEmailTitle:
     "Välkommen till {{vendorName}}! {{code}} är koden för att logga in",
-  codeEmailWelcomeToYourAccount: "Välkommen till ditt {{vendorName}} konto!",
   codeEmailEnterCode:
     "Skriv in koden på {{vendorName}} för att slutföra inloggningen",
   codeEmailCodeValid30Mins: "Koden är giltig i 30 minuter.",
   codeEmailSupportInfo:
     "Om du har frågor eller behöver assistans kan du kontakta vårt supportteam",
+  // link email
+  linkEmailClickToLogin: "Klicka på knappen för att logga in",
+  linkEmailLogin: "Logga in",
+  linkEmailOrEnterCode:
+    "  Eller skriv in koden på {{vendorName}} för att slutföra inloggningen.",
+  // password reset email
   passwordResetTitle: "Byt lösenord för ditt {{vendorName}} konto",
-  contactUs: "Kontakta oss",
-  copyright: "Copyright © 2023 SESAMY. Alla rättigheter förbehållna.",
 };
