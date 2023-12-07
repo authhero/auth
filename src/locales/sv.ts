567dbbabdb208891d2301746e901d75b8fa9a73e
Daniel Gent <dan@sesamy.com>
2023-12-07 17:37:10 +0100

    feat: translate strings to english

--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -3,19 +3,19 @@ export const sv = {
   welcomeToYourAccount: "Välkommen till ditt {{vendorName}} konto!",
   contactUs: "Kontakta oss",
   copyright: "Copyright © 2023 SESAMY. Alla rättigheter förbehållna.",
+  codeValid30Mins: "Koden är giltig i 30 minuter.",
+  supportInfo:
+    "Om du har frågor eller behöver assistans kan du kontakta vårt supportteam",
   // code email
   codeEmailTitle:
     "Välkommen till {{vendorName}}! {{code}} är koden för att logga in",
   codeEmailEnterCode:
     "Skriv in koden på {{vendorName}} för att slutföra inloggningen",
-  codeEmailCodeValid30Mins: "Koden är giltig i 30 minuter.",
-  codeEmailSupportInfo:
-    "Om du har frågor eller behöver assistans kan du kontakta vårt supportteam",
   // link email
   linkEmailClickToLogin: "Klicka på knappen för att logga in",
   linkEmailLogin: "Logga in",
   linkEmailOrEnterCode:
-    "  Eller skriv in koden på {{vendorName}} för att slutföra inloggningen.",
+    "Eller skriv in koden på {{vendorName}} för att slutföra inloggningen.",
   // password reset email
   passwordResetTitle: "Byt lösenord för ditt {{vendorName}} konto",
 };
