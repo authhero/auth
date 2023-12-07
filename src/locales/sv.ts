1ac3a653bd99ef0e7ae44e944ec5b8fcf2d4d6fd
Daniel Gent <dan@sesamy.com>
2023-12-07 17:13:41 +0100

    fest: extract out all swedish text from code email

--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -2,5 +2,12 @@ export const sv = {
   codeEmailTitle:
     "Välkommen till {{vendorName}}! {{code}} är koden för att logga in",
   codeEmailWelcomeToYourAccount: "Välkommen till ditt {{vendorName}} konto!",
+  codeEmailEnterCode:
+    "Skriv in koden på {{vendorName}} för att slutföra inloggningen",
+  codeEmailCodeValid30Mins: "Koden är giltig i 30 minuter.",
+  codeEmailSupportInfo:
+    "Om du har frågor eller behöver assistans kan du kontakta vårt supportteam",
   passwordResetTitle: "Byt lösenord för ditt {{vendorName}} konto",
+  contactUs: "Kontakta oss",
+  copyright: "Copyright © 2023 SESAMY. Alla rättigheter förbehållna.",
 };
