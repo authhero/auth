5787f9511b541660ea89b9111ce60b6fc390c708
Markus Ahlstrand <markus@ahlstrand.de>
2023-12-08 16:13:18 +0100

    fix: add index for user linked to

---
+++ migrate/migrations/2023-12-08T15:59:00_user-linked-to-index.ts
@@ -0,0 +1,14 @@
+import { Kysely } from "kysely";
+import { Database } from "../../src/types";
+
+export async function up(db: Kysely<Database>): Promise<void> {
+  await db.schema
+    .createIndex("users_linked_to_index")
+    .on("users")
+    .column("linked_to")
+    .execute();
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  await db.schema.dropIndex("schema");
+}
