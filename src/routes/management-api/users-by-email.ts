4612b2a273e01450a92301b7f7eaad5e6cfbcd12
Daniel Gent <dan@sesamy.com>
2023-12-08 12:28:07 +0100

    feat: return single email user in array

--- src/routes/management-api/users-by-email.ts
+++ src/routes/management-api/users-by-email.ts
@@ -27,7 +27,8 @@ export class UsersByEmailController extends Controller {
     @Request() request: RequestWithContext,
     @Query("email") email: string,
     @Header("tenant-id") tenant_id: string,
-  ): Promise<User> {
+  ): Promise<User[]> {
+    // This should be ResponseUser!  8-)
     const { env } = request.ctx;
 
     const user = await env.data.users.getByEmail(tenant_id, email);
@@ -35,6 +36,11 @@ export class UsersByEmailController extends Controller {
       throw new HTTPException(404, { message: "User not found" });
     }
 
-    return user;
+    // Step 1 - return this user in array
+    // Step 2 - return all users with this email address
+    // Step 3- filter out linked users e.g. linked_to field is populated
+    // Step 4 - return nested identities
+    // don't overthink this last step. copy-paste but have tests so can TDD
+    return [user];
   }
 }
