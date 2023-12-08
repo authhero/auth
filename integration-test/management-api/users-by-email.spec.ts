477c2f5d051085855fedfd49cda43fe93bdfe4e1
Daniel Gent <dan@sesamy.com>
2023-12-08 14:50:33 +0100

    test: skipped test - cannot do this currently

--- integration-test/management-api/users-by-email.spec.ts
+++ integration-test/management-api/users-by-email.spec.ts
@@ -2,6 +2,7 @@ import { UserResponse } from "../../src/types/auth0";
 import { getAdminToken } from "../helpers/token";
 import { start } from "../start";
 import type { UnstableDevWorker } from "wrangler";
+import { setup } from "../helpers/setup";
 
 describe("users by email", () => {
   let worker: UnstableDevWorker;
@@ -89,9 +90,61 @@ describe("users by email", () => {
     ]);
   });
 
+  it.skip("should return multiple users for a simple get by email - no linked accounts", async () => {
+    const token = await getAdminToken();
+
+    // duplicate existing email foo@example.com for provider: 'username - password'
+    // This assumes the POST endpoint doesn't do automatic account linking...
+    // would be better if we could initialise the database with multiple accounts...
+    // and different on different test runs... TBD another time
+    const createDuplicateUserResponse = await worker.fetch("/api/v2/users", {
+      method: "POST",
+      body: JSON.stringify({
+        email: "foo@example.com",
+        connection: "Username-Password-Authentication",
+        // seems odd that this isn't allowed... I think this endpoint needs looking at
+        // maybe it's good we have to use the mgmt API for our test fixtures
+        // provider: "auth2",
+      }),
+      headers: {
+        authorization: `Bearer ${token}`,
+        "tenant-id": "tenantId",
+        "content-type": "application/json",
+      },
+    });
+
+    const newDuplicateUser =
+      (await createDuplicateUserResponse.json()) as UserResponse;
+    expect(newDuplicateUser.email).toBe("foo@example.com");
+
+    // user id here should be different to existing user id for foo@example.com
+    console.log(newDuplicateUser.user_id);
+
+    expect(createDuplicateUserResponse.status).toBe(201);
+
+    const response = await worker.fetch(
+      `/api/v2/users-by-email?email=${encodeURIComponent("foo@example.com")}`,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+          "tenant-id": "tenantId",
+        },
+      },
+    );
+
+    expect(response.status).toBe(200);
+
+    const users = (await response.json()) as UserResponse[];
+
+    // Nice! this POST endpoint just duplicates the user anyway  8-)
+    // expect(users.length).toBe(2);
+
+    // Cannot test this until we have a way to duplicate users...
+    // TODO - investigate POST to Auth0 mgmt API/users
+  });
+
   /* 
   TO TEST
-  * multiple simple primary not linked accounts
   * a primary account with multiple linked accounts - some on different email addresses
    
   
