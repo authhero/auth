32f779144b7c123f7a70756f4ad68a33496cd749
Daniel Gent <dan@sesamy.com>
2023-12-14 21:20:33 +0100

    test: create helper silent auth token extractor

---
+++ integration-test/helpers/silent-auth.ts
@@ -0,0 +1,73 @@
+import { parseJwt } from "../../src/utils/parse-jwt";
+
+import type { UnstableDevWorker } from "wrangler";
+
+async function doTheSilentAuthThing(
+  setCookiesHeader: string,
+  worker: UnstableDevWorker,
+  nonce: string,
+) {
+  const cookies = setCookiesHeader.split(";").map((c) => c.trim());
+  const authCookie = cookies.find((c) => c.startsWith("auth-token"))!;
+
+  const silentAuthSearchParams = new URLSearchParams();
+  silentAuthSearchParams.set("client_id", "clientId");
+  silentAuthSearchParams.set("response_type", "token id_token");
+  silentAuthSearchParams.set("scope", "openid profile email");
+  silentAuthSearchParams.set("redirect_uri", "http://localhost:3000/callback");
+  silentAuthSearchParams.set("state", "state");
+  // silent auth pararms!
+  silentAuthSearchParams.set("prompt", "none");
+  silentAuthSearchParams.set("nonce", nonce);
+  silentAuthSearchParams.set("response_mode", "web_message");
+
+  const silentAuthResponse = await worker.fetch(
+    `/authorize?${silentAuthSearchParams.toString()}`,
+    {
+      headers: {
+        // here we set the auth cookie given to us from the previous successful auth request
+        cookie: authCookie,
+      },
+    },
+  );
+
+  const body = await silentAuthResponse.text();
+
+  expect(body).not.toContain("Login required");
+
+  expect(body).toContain("access_token");
+
+  // get id token from iframe response body
+  const lines = body.split("\n");
+  const responseBody = lines.find((line) =>
+    line.trim().startsWith("response: "),
+  );
+  if (!responseBody) {
+    throw new Error("iframe auth body missing");
+  }
+
+  const iframeResponseJSON = JSON.parse(responseBody.replace("response: ", ""));
+
+  const silentAuthIdToken = iframeResponseJSON.id_token;
+
+  const silentAuthIdTokenPayload = parseJwt(silentAuthIdToken);
+
+  const {
+    // these are the fields that change on every test run
+    exp,
+    iat,
+    sid,
+    sub,
+    ...restOfIdTokenPayload
+  } = silentAuthIdTokenPayload;
+
+  const silentAuthToken = iframeResponseJSON.access_token;
+
+  const silentAuthTokenPayload = parseJwt(silentAuthToken);
+
+  // these aren't typed! coming out as anys...
+  return {
+    accessToken: silentAuthTokenPayload,
+    idToken: restOfIdTokenPayload,
+  };
+}
