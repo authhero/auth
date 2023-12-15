d65f828c3e7f477b616be2cf7f8f46485022bb16
Daniel Gent <dan@sesamy.com>
2023-12-15 14:50:17 +0100

    test: use silent auth util in more places

--- integration-test/flows/code-flow.spec.ts
+++ integration-test/flows/code-flow.spec.ts
@@ -124,55 +124,14 @@ describe("code-flow", () => {
     // now check silent auth works when logged in with code----------------------------------------
     const setCookiesHeader = tokenResponse.headers.get("set-cookie")!;
 
-    const cookies = setCookiesHeader.split(";").map((c) => c.trim());
-    const authCookie = cookies.find((c) => c.startsWith("auth-token"))!;
-
-    const silentAuthSearchParams = new URLSearchParams();
-    silentAuthSearchParams.set("client_id", "clientId");
-    silentAuthSearchParams.set("response_type", "token id_token");
-    silentAuthSearchParams.set("scope", "openid profile email");
-    silentAuthSearchParams.set(
-      "redirect_uri",
-      "http://localhost:3000/callback",
+    const {
+      accessToken: silentAuthAccessTokenPayload,
+      idToken: silentAuthIdTokenPayload,
+    } = await doSilentAuthRequestAndReturnTokens(
+      setCookiesHeader,
+      worker,
+      nonce,
     );
-    silentAuthSearchParams.set("state", "state");
-    // silent auth pararms!
-    silentAuthSearchParams.set("prompt", "none");
-    silentAuthSearchParams.set("nonce", nonce);
-    silentAuthSearchParams.set("response_mode", "web_message");
-
-    const silentAuthResponse = await worker.fetch(
-      `/authorize?${silentAuthSearchParams.toString()}`,
-      {
-        headers: {
-          // here we set the auth cookie given to us from the previous successful auth request
-          cookie: authCookie,
-        },
-      },
-    );
-
-    const body = await silentAuthResponse.text();
-
-    expect(body).not.toContain("Login required");
-
-    expect(body).toContain("access_token");
-
-    // get id token from iframe response body
-    const lines = body.split("\n");
-    const responseBody = lines.find((line) =>
-      line.trim().startsWith("response: "),
-    );
-    if (!responseBody) {
-      throw new Error("iframe auth body missing");
-    }
-
-    const iframeResponseJSON = JSON.parse(
-      responseBody.replace("response: ", ""),
-    );
-
-    const silentAuthIdToken = iframeResponseJSON.id_token;
-
-    const silentAuthIdTokenPayload = parseJwt(silentAuthIdToken);
 
     const {
       // these are the fields that change on every test run
