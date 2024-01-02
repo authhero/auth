eec1bf680660da3caff196b50e2bf062e8abce87
Daniel Gent <dan@sesamy.com>
2024-01-02 15:17:35 +0000

    test: addd default user fixture

--- test/integration/helpers/test-client.ts
+++ test/integration/helpers/test-client.ts
@@ -155,6 +155,25 @@ export async function getEnv() {
   data.connections.create("tenantId", testConnection1);
   data.connections.create("tenantId", testConnection2);
 
+  data.users.create("tenantId", {
+    // my test correctly informs this is not a valid user_id!
+    // TODO - fix this in another PR? or do we want backwards compatibility?
+    id: "userId",
+    email: "foo@example.com",
+    email_verified: true,
+    name: "Åkesson Þorsteinsson",
+    // use a norse nickname here? more realistic
+    nickname: "Åkesson Þorsteinsson",
+    picture: "https://example.com/foo.png",
+    tenant_id: "tenantId",
+    login_count: 0,
+    provider: "auth2",
+    connection: "Username-Password-Authentication",
+    is_social: false,
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+  });
+
   return {
     data,
     JWKS_URL: "https://example.com/.well-known/jwks.json",
