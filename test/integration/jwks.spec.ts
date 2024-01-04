c858ee835e48d40a5e0f7d05746d0ef0bcff3c07
Daniel Gent <dan@sesamy.com>
2024-01-04 17:08:25 +0000

    test: fix next test but its not returning the newly created key...

--- test/integration/jwks.spec.ts
+++ test/integration/jwks.spec.ts
@@ -11,8 +11,6 @@ describe("jwks", () => {
     const env = await getEnv();
     const client = testClient(tsoaApp, env);
 
-    // const response = await worker.fetch("/.well-known/jwks.json");
-
     const response = await client[".well-known"]["jwks.json"].$get(
       {},
       {
@@ -28,28 +26,38 @@ describe("jwks", () => {
     expect(body.keys.length).toBe(1);
   });
 
-  // it("should create a new rsa-key and return it", async () => {
-  //   const token = await getAdminToken();
-  //   const createKeyResponse = await worker.fetch(
-  //     "/api/v2/keys/signing/rotate",
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "tenant-id": "tenantId",
-  //         authorization: `Bearer ${token}`,
-  //       },
-  //     },
-  //   );
+  it("should create a new rsa-key and return it", async () => {
+    const env = await getEnv();
+    const client = testClient(tsoaApp, env);
+    const token = await getAdminToken();
 
-  //   expect(createKeyResponse.status).toBe(201);
+    const createKeyResponse = await client.api.v2.keys.signing.rotate.$post(
+      {},
+      {
+        headers: {
+          "tenant-id": "tenantId",
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
 
-  //   const response = await worker.fetch("/.well-known/jwks.json");
+    expect(createKeyResponse.status).toBe(201);
 
-  //   expect(response.status).toBe(200);
+    const response = await client[".well-known"]["jwks.json"].$get(
+      {},
+      {
+        headers: {
+          "tenant-id": "tenantId",
+        },
+      },
+    );
 
-  //   const body = (await response.json()) as Jwks[];
-  //   expect(body.keys.length).toBe(2);
-  // });
+    expect(response.status).toBe(200);
+
+    const body = (await response.json()) as Jwks[];
+
+    expect(body.keys.length).toBe(2);
+  });
 
   // it("should return an openid-configuration with the current issues", async () => {
   //   const response = await worker.fetch("/.well-known/openid-configuration");
