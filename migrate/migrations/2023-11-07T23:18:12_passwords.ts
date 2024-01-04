d07a624d618cfb4125f20f109e2fcf898a22bf00
Daniel Gent <dan@sesamy.com>
2024-01-04 12:34:44 +0000

    fix: add cascading delete to all fks

--- migrate/migrations/2023-11-07T23:18:12_passwords.ts
+++ migrate/migrations/2023-11-07T23:18:12_passwords.ts
@@ -17,7 +17,7 @@ export async function up(db: Kysely<Database>): Promise<void> {
       ["user_id", "tenant_id"],
       "users",
       ["id", "tenant_id"],
-      // TODO - add cascading delete on all the addForeignKeyConstraint
+      (cb) => cb.onDelete("cascade"),
     )
     .addColumn("created_at", "varchar(255)")
     .addColumn("updated_at", "varchar(255)")
