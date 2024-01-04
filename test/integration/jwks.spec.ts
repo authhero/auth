2aa8745730b0cfd7bde5bd7baf71cac2651ff14c
Daniel Gent <dan@sesamy.com>
2024-01-04 17:03:49 +0000

    test: nope, this does not work

--- test/integration/jwks.spec.ts
+++ test/integration/jwks.spec.ts
@@ -1,22 +1,26 @@
-import { OpenIDConfiguration } from "../src/routes/tsoa/jwks";
-import { Jwks } from "../src/types/jwks";
-import { getAdminToken } from "./helpers/token";
-import { start } from "./start";
-import type { UnstableDevWorker } from "wrangler";
+import { OpenIDConfiguration } from "../../src/routes/tsoa/jwks";
+import { Jwks } from "../../src/types/jwks";
+// let's move this into a common folder?
+import { getAdminToken } from "../../integration-test/helpers/token";
+import { getEnv } from "./helpers/test-client";
+import { tsoaApp } from "../../src/app";
+import { testClient } from "hono/testing";
 
 describe("jwks", () => {
-  let worker: UnstableDevWorker;
-
-  beforeEach(async () => {
-    worker = await start();
-  });
-
-  afterEach(() => {
-    worker.stop();
-  });
-
   it("should return a list with the test certificate", async () => {
-    const response = await worker.fetch("/.well-known/jwks.json");
+    const env = await getEnv();
+    const client = testClient(tsoaApp, env);
+
+    // const response = await worker.fetch("/.well-known/jwks.json");
+
+    const response = await client.api.v2["jwks"].$get(
+      {},
+      {
+        headers: {
+          "tenant-id": "tenantId",
+        },
+      },
+    );
 
     expect(response.status).toBe(200);
 
@@ -24,35 +28,35 @@ describe("jwks", () => {
     expect(body.keys.length).toBe(1);
   });
 
-  it("should create a new rsa-key and return it", async () => {
-    const token = await getAdminToken();
-    const createKeyResponse = await worker.fetch(
-      "/api/v2/keys/signing/rotate",
-      {
-        method: "POST",
-        headers: {
-          "tenant-id": "tenantId",
-          authorization: `Bearer ${token}`,
-        },
-      },
-    );
+  // it("should create a new rsa-key and return it", async () => {
+  //   const token = await getAdminToken();
+  //   const createKeyResponse = await worker.fetch(
+  //     "/api/v2/keys/signing/rotate",
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "tenant-id": "tenantId",
+  //         authorization: `Bearer ${token}`,
+  //       },
+  //     },
+  //   );
 
-    expect(createKeyResponse.status).toBe(201);
+  //   expect(createKeyResponse.status).toBe(201);
 
-    const response = await worker.fetch("/.well-known/jwks.json");
+  //   const response = await worker.fetch("/.well-known/jwks.json");
 
-    expect(response.status).toBe(200);
+  //   expect(response.status).toBe(200);
 
-    const body = (await response.json()) as Jwks[];
-    expect(body.keys.length).toBe(2);
-  });
+  //   const body = (await response.json()) as Jwks[];
+  //   expect(body.keys.length).toBe(2);
+  // });
 
-  it("should return an openid-configuration with the current issues", async () => {
-    const response = await worker.fetch("/.well-known/openid-configuration");
+  // it("should return an openid-configuration with the current issues", async () => {
+  //   const response = await worker.fetch("/.well-known/openid-configuration");
 
-    expect(response.status).toBe(200);
+  //   expect(response.status).toBe(200);
 
-    const body = (await response.json()) as OpenIDConfiguration;
-    expect(body.issuer).toBe("https://example.com/");
-  });
+  //   const body = (await response.json()) as OpenIDConfiguration;
+  //   expect(body.issuer).toBe("https://example.com/");
+  // });
 });
