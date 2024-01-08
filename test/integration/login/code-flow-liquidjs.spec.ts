308d76d8ca547bb963f94d077f7fd727d4d7bbe9
Daniel Gent <dan@sesamy.com>
2024-01-08 15:16:01 +0000

    test: migrate first part of liquidjs login test to hono

--- test/integration/login/code-flow-liquidjs.spec.ts
+++ test/integration/login/code-flow-liquidjs.spec.ts
@@ -1,34 +1,23 @@
-import { start } from "../start";
-import type { UnstableDevWorker } from "wrangler";
+import { getEnv } from "../helpers/test-client";
+import { tsoaApp } from "../../../src/app";
+import { testClient } from "hono/testing";
 
 describe("Login with code on liquidjs template", () => {
-  let worker: UnstableDevWorker;
-
-  beforeEach(async () => {
-    worker = await start();
-  });
-
-  afterEach(() => {
-    worker.stop();
-  });
-
   it("should login with code", async () => {
-    const searchParams = new URLSearchParams();
+    const env = await getEnv();
+    const client = testClient(tsoaApp, env);
 
-    searchParams.set("client_id", "clientId");
-    searchParams.set("response_type", "token id_token");
-    searchParams.set("scope", "openid");
-    searchParams.set("redirect_uri", "http://localhost:3000/callback");
-    searchParams.set("state", "state");
-    const response = await worker.fetch(
-      `/authorize?${searchParams.toString()}`,
-      {
-        headers: {
-          "tenant-id": "tenantId",
-        },
-        redirect: "manual",
-      },
-    );
+    const searchParams = {
+      client_id: "clientId",
+      response_type: "token id_token",
+      scope: "openid",
+      redirect_uri: "http://localhost:3000/callback",
+      state: "state",
+    };
+
+    const response = await client.authorize.$get({
+      query: searchParams,
+    });
 
     expect(response.status).toBe(302);
     const location = response.headers.get("location");
@@ -40,62 +29,62 @@ describe("Login with code on liquidjs template", () => {
     expect(location.startsWith("/u/login")).toBeTruthy;
 
     // Open send code page - would be cool to get the URL from the login page template to test that we're passing in the state correctly
-    const stateParam = new URLSearchParams(location.split("?")[1]);
+    // const stateParam = new URLSearchParams(location.split("?")[1]);
 
-    const postSendCodeResponse = await worker.fetch(
-      `/u/code?${stateParam.toString()}`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          username: "foo@example.com",
-        }),
-        headers: {
-          "content-type": "application/json",
-        },
-        redirect: "manual",
-      },
-    );
+    // const postSendCodeResponse = await worker.fetch(
+    //   `/u/code?${stateParam.toString()}`,
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       username: "foo@example.com",
+    //     }),
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     redirect: "manual",
+    //   },
+    // );
 
-    expect(postSendCodeResponse.status).toBe(302);
-    const enterCodeLocation = postSendCodeResponse.headers.get("location");
+    // expect(postSendCodeResponse.status).toBe(302);
+    // const enterCodeLocation = postSendCodeResponse.headers.get("location");
 
-    if (!enterCodeLocation) {
-      throw new Error("No login location header found");
-    }
+    // if (!enterCodeLocation) {
+    //   throw new Error("No login location header found");
+    // }
 
-    const enterCodeParams = enterCodeLocation.split("?")[1];
+    // const enterCodeParams = enterCodeLocation.split("?")[1];
 
-    const emailResponse = await worker.fetch("/test/email");
-    // would be cool to have proper types returned from this...
-    const [sentEmail]: any = await emailResponse.json();
-    expect(sentEmail.to).toBe("foo@example.com");
+    // const emailResponse = await worker.fetch("/test/email");
+    // // would be cool to have proper types returned from this...
+    // const [sentEmail]: any = await emailResponse.json();
+    // expect(sentEmail.to).toBe("foo@example.com");
 
-    const code = sentEmail.code;
+    // const code = sentEmail.code;
 
-    // Authenticate using the code
-    const authenticateResponse = await worker.fetch(
-      `/u/enter-code?${enterCodeParams}`,
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({
-          code,
-        }),
-        redirect: "manual",
-      },
-    );
+    // // Authenticate using the code
+    // const authenticateResponse = await worker.fetch(
+    //   `/u/enter-code?${enterCodeParams}`,
+    //   {
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       code,
+    //     }),
+    //     redirect: "manual",
+    //   },
+    // );
 
-    const codeLoginRedirectUri = authenticateResponse.headers.get("location");
-    if (!codeLoginRedirectUri) {
-      throw new Error("No code login redirect uri found");
-    }
-    const redirectUrl = new URL(codeLoginRedirectUri);
-    expect(redirectUrl.pathname).toBe("/callback");
-    const accessToken = redirectUrl.searchParams.get("access_token");
-    expect(accessToken).toBeTruthy();
-    const idToken = redirectUrl.searchParams.get("id_token");
-    expect(idToken).toBeTruthy();
+    // const codeLoginRedirectUri = authenticateResponse.headers.get("location");
+    // if (!codeLoginRedirectUri) {
+    //   throw new Error("No code login redirect uri found");
+    // }
+    // const redirectUrl = new URL(codeLoginRedirectUri);
+    // expect(redirectUrl.pathname).toBe("/callback");
+    // const accessToken = redirectUrl.searchParams.get("access_token");
+    // expect(accessToken).toBeTruthy();
+    // const idToken = redirectUrl.searchParams.get("id_token");
+    // expect(idToken).toBeTruthy();
   });
 });
