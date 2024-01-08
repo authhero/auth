e08dad50aa604ffe9ded102938cff704d755d430
Daniel Gent <dan@sesamy.com>
2024-01-08 17:19:59 +0000

    test: migrate first part of test

--- test/integration/login/login-password.spec.ts
+++ test/integration/login/login-password.spec.ts
@@ -1,34 +1,23 @@
-import { start } from "../start";
-import type { UnstableDevWorker } from "wrangler";
+import { getEnv } from "../helpers/test-client";
+import { tsoaApp } from "../../../src/app";
+import { testClient } from "hono/testing";
 
 describe("Login with password user", () => {
-  let worker: UnstableDevWorker;
-
-  beforeEach(async () => {
-    worker = await start();
-  });
-
-  afterEach(() => {
-    worker.stop();
-  });
-
   it("should login with password", async () => {
-    const searchParams = new URLSearchParams();
+    const env = await getEnv();
+    const client = testClient(tsoaApp, env);
 
-    searchParams.set("client_id", "clientId");
-    searchParams.set("response_type", "token id_token");
-    searchParams.set("scope", "openid");
-    searchParams.set("redirect_uri", "http://localhost:3000/callback");
-    searchParams.set("state", "state");
-    const response = await worker.fetch(
-      `/authorize?${searchParams.toString()}`,
-      {
-        headers: {
-          "tenant-id": "tenantId",
-        },
-        redirect: "manual",
-      },
-    );
+    const searchParams = {
+      client_id: "clientId",
+      response_type: "token id_token",
+      scope: "openid",
+      redirect_uri: "http://localhost:3000/callback",
+      state: "state",
+    };
+
+    const response = await client.authorize.$get({
+      query: searchParams,
+    });
 
     expect(response.status).toBe(302);
     const location = response.headers.get("location");
@@ -39,38 +28,47 @@ describe("Login with password user", () => {
 
     expect(location.startsWith("/u/login")).toBeTruthy;
 
+    console.log(location);
+
+    const stateParam = new URLSearchParams(location.split("?")[1]);
+    const query = Object.fromEntries(stateParam.entries());
+
     // Open login page
-    const loginFormResponse = await worker.fetch(location, {});
+    const loginFormResponse = await client.u.login.$get({
+      query,
+    });
+
+    console.log(await loginFormResponse.text());
     expect(loginFormResponse.status).toBe(200);
-    const loginSearchParams = new URLSearchParams(location.split("?")[1]);
+    // const loginSearchParams = new URLSearchParams(location.split("?")[1]);
 
-    const postLoginResponse = await worker.fetch(
-      `/u/login?${loginSearchParams.toString()}`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          username: "foo@example.com",
-          password: "Test!",
-        }),
-        headers: {
-          "content-type": "application/json",
-        },
-        redirect: "manual",
-      },
-    );
+    // const postLoginResponse = await worker.fetch(
+    //   `/u/login?${loginSearchParams.toString()}`,
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       username: "foo@example.com",
+    //       password: "Test!",
+    //     }),
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     redirect: "manual",
+    //   },
+    // );
 
-    expect(postLoginResponse.status).toBe(302);
-    const loginLocation = postLoginResponse.headers.get("location");
+    // expect(postLoginResponse.status).toBe(302);
+    // const loginLocation = postLoginResponse.headers.get("location");
 
-    if (!loginLocation) {
-      throw new Error("No login location header found");
-    }
+    // if (!loginLocation) {
+    //   throw new Error("No login location header found");
+    // }
 
-    const redirectUrl = new URL(loginLocation);
-    expect(redirectUrl.pathname).toBe("/callback");
-    const accessToken = redirectUrl.searchParams.get("access_token");
-    expect(accessToken).toBeTruthy();
-    const idToken = redirectUrl.searchParams.get("id_token");
-    expect(idToken).toBeTruthy();
+    // const redirectUrl = new URL(loginLocation);
+    // expect(redirectUrl.pathname).toBe("/callback");
+    // const accessToken = redirectUrl.searchParams.get("access_token");
+    // expect(accessToken).toBeTruthy();
+    // const idToken = redirectUrl.searchParams.get("id_token");
+    // expect(idToken).toBeTruthy();
   });
 });
