c27f1bbadf87a1e7338b7710ce617d93b9af1632
Daniel Gent <dan@sesamy.com>
2024-02-02 16:35:27 +0200

    chore: add tenant & date indexes to logs table

--- migrate/migrations/2024-02-02T16:55:00_logs-indexes.ts
+++ migrate/migrations/2024-02-02T16:55:00_logs-indexes.ts
@@ -7,8 +7,18 @@ export async function up(db: Kysely<Database>): Promise<void> {
     .on("logs")
     .column("user_id")
     .execute();
+
+  await db.schema
+    .createIndex("logs_tenant_id")
+    .on("logs")
+    .column("tenant_id")
+    .execute();
+
+  await db.schema.createIndex("logs_date").on("logs").column("date").execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   await db.schema.dropIndex("logs_user_id");
+  await db.schema.dropIndex("logs_tenant_id");
+  await db.schema.dropIndex("logs_date");
 }
