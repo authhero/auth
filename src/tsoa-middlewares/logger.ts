f1d8cfbfec06a16dc75a1c1adec23e937c452124
Daniel Gent <dan@sesamy.com>
2024-02-07 21:30:59 +0200

    fix: types after rebase

--- src/tsoa-middlewares/logger.ts
+++ src/tsoa-middlewares/logger.ts
@@ -45,23 +45,24 @@ export function loggerMiddleware(logType: string, description?: string) {
         console.error(e);
       }
 
-    try {
-      await env.data.logs.create(ctx.var.tenantId || "", {
-        user_id: ctx.var.userId || "",
-        description: ctx.var.description || description || "",
-        ip: ctx.req.header("x-real-ip") || "",
-        type: ctx.var.logType || logType,
-        client_id: ctx.var.client_id,
-        client_name: "",
-        user_agent: ctx.req.header("user-agent"),
-        date: new Date().toISOString(),
-        details: {
-          request: {
-            method: ctx.req.method,
-            path: ctx.req.path,
-            headers: instanceToJson(ctx.req.raw.headers),
-            qs: ctx.req.queries(),
-            body,
+      try {
+        await env.data.logs.create(ctx.var.tenantId || "", {
+          user_id: ctx.var.userId || "",
+          description: ctx.var.description || description || "",
+          ip: ctx.req.header("x-real-ip") || "",
+          type: ctx.var.logType || logType,
+          client_id: ctx.var.client_id,
+          client_name: "",
+          user_agent: ctx.req.header("user-agent"),
+          date: new Date().toISOString(),
+          details: {
+            request: {
+              method: ctx.req.method,
+              path: ctx.req.path,
+              headers: instanceToJson(ctx.req.raw.headers),
+              qs: ctx.req.queries(),
+              body,
+            },
           },
         });
       } catch (e) {
@@ -84,8 +85,7 @@ export function loggerMiddleware(logType: string, description?: string) {
 
       if (e instanceof HTTPException) {
         try {
-          await env.data.logs.create({
-            tenant_id: ctx.var.tenantId,
+          await env.data.logs.create(ctx.var.tenantId || "", {
             user_id: ctx.var.userId || "",
             description: e.message || ctx.var.description || description || "",
             ip: ctx.req.header("x-real-ip") || "",
