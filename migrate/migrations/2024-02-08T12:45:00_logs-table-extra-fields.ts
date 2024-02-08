984115cda80d14fefea8ec9e46bcade35f967538
Daniel Gent <dan@sesamy.com>
2024-02-08 17:06:14 +0200

    fix: add new required sqlite fields

--- migrate/migrations/2024-02-08T12:45:00_logs-table-extra-fields.ts
+++ migrate/migrations/2024-02-08T12:45:00_logs-table-extra-fields.ts
@@ -5,12 +5,32 @@ export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .alterTable("logs")
     .addColumn("user_name", "varchar(255)")
-    // TODO - include other fields now!
     .execute();
+
+  // I do not think this will be long enough
+  await db.schema
+    .alterTable("logs")
+    .addColumn("auth0_client", "varchar(255)")
+    .execute();
+  await db.schema.alterTable("logs").addColumn("isMobile", "boolean").execute;
+  await db.schema.alterTable("logs").addColumn("connection", "varchar(255)")
+    .execute;
+  await db.schema.alterTable("logs").addColumn("connection_id", "varchar(255)")
+    .execute;
+  await db.schema.alterTable("logs").addColumn("audience", "varchar(255)")
+    .execute;
+  await db.schema.alterTable("logs").addColumn("scope", "varchar(255)").execute;
+  await db.schema.alterTable("logs").addColumn("strategy", "varchar(255)")
+    .execute;
+  await db.schema.alterTable("logs").addColumn("strategy_type", "varchar(255)")
+    .execute;
+  await db.schema.alterTable("logs").addColumn("hostname", "varchar(255)")
+    .execute;
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   await db.schema.alterTable("logs").dropColumn("details").execute();
 
   await db.schema.alterTable("logs").dropColumn("user_name");
+  // TODO - add these here too!
 }
