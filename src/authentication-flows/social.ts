f85858143e4d4f6766c81217bd406c9facc77d32
Daniel Gent <dan@sesamy.com>
2024-02-13 12:28:50 +0200

    Revert "chore: remove all unrelated parts of PR for review"
    This reverts commit 42de60e58f81b4f07cb0743d0de86caaf2a23c31.

--- src/authentication-flows/social.ts
+++ src/authentication-flows/social.ts
@@ -30,6 +30,7 @@ export async function socialAuth(
     (p) => p.name === connection,
   );
   if (!connectionInstance) {
+    // cannot actually set a different log type here...
     throw new HTTPException(403, { message: "Connection Not Found" });
   }
 
