53027ae049eb6a767c1e11ea57763b43af3e6585
Daniel Gent <dan@sesamy.com>
2024-02-13 14:26:09 +0200

    fix: remove connection id

--- src/types/Var.ts
+++ src/types/Var.ts
@@ -15,7 +15,8 @@ export type Var = {
     permissions: string[];
   };
   userName?: string;
-  connectionId?: string;
+  // I'm not sure what connection_id actually means here...  In Auth0 we get con_TI7p6dEHf551Q9t6
+  // connectionId?: string;
   connection?: string;
   // TODO - enable these, populate, and use
   //   strategy?: string;
