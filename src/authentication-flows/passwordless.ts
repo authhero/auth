f8fd5e07779732e40baa09ea8909a0929bf42347
Daniel Gent <dan@sesamy.com>
2024-02-16 20:39:17 +0200

    feat: attempt a flow but I do not know

--- src/authentication-flows/passwordless.ts
+++ src/authentication-flows/passwordless.ts
@@ -2,7 +2,8 @@ import { HTTPException } from "hono/http-exception";
 import { Env } from "../types";
 import userIdGenerate from "../utils/userIdGenerate";
 import { getClient } from "../services/clients";
-import { getPrimaryUserByEmailAndProvider } from "../utils/users";
+import { getUsersByEmail } from "../utils/users";
+import { User } from "../types";
 
 interface LoginParams {
   client_id: string;
@@ -10,7 +11,10 @@ interface LoginParams {
   verification_code: string;
 }
 
-export async function validateCode(env: Env, params: LoginParams) {
+export async function validateCode(
+  env: Env,
+  params: LoginParams,
+): Promise<User> {
   const client = await getClient(env, params.client_id);
   if (!client) {
     throw new HTTPException(400, { message: "Client not found" });
@@ -23,15 +27,36 @@ export async function validateCode(env: Env, params: LoginParams) {
     throw new HTTPException(403, { message: "Code not found or expired" });
   }
 
-  let user = await getPrimaryUserByEmailAndProvider({
-    userAdapter: env.data.users,
-    tenant_id: client.tenant_id,
-    email: params.email,
-    provider: "email",
-  });
+  const usersWithSameEmailAddress = await getUsersByEmail(
+    env.data.users,
+    client.tenant_id,
+    params.email,
+  );
 
-  if (!user) {
-    user = await env.data.users.create(client.tenant_id, {
+  const emailUser = usersWithSameEmailAddress.find(
+    (user) => user.provider === "email",
+  );
+
+  if (emailUser) {
+    if (!!emailUser.linked_to) {
+      const primaryUser = await env.data.users.get(
+        client.tenant_id,
+        emailUser.linked_to,
+      );
+
+      if (!primaryUser) {
+        throw new HTTPException(400, { message: "Primary user not found" });
+      }
+
+      return primaryUser;
+    }
+    return emailUser;
+  }
+
+  const primaryUser = usersWithSameEmailAddress.find((u) => !u.linked_to);
+
+  if (primaryUser) {
+    await env.data.users.create(client.tenant_id, {
       id: `email|${userIdGenerate()}`,
       email: params.email,
       name: params.email,
@@ -44,8 +69,26 @@ export async function validateCode(env: Env, params: LoginParams) {
       is_social: false,
       created_at: new Date().toISOString(),
       updated_at: new Date().toISOString(),
+      linked_to: primaryUser.id,
     });
+
+    return primaryUser;
   }
 
-  return user;
+  const newUser = await env.data.users.create(client.tenant_id, {
+    id: `email|${userIdGenerate()}`,
+    email: params.email,
+    name: params.email,
+    provider: "email",
+    connection: "email",
+    email_verified: true,
+    last_ip: "",
+    login_count: 1,
+    last_login: new Date().toISOString(),
+    is_social: false,
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+  });
+
+  return newUser;
 }
