c03b36583427106940b8b5f7d3189529f10b418f
Daniel Gent <dan@sesamy.com>
2024-03-08 18:14:18 +0200

    feat: create template & add to cloudflare

--- scripts/create-mjml-templates.sh
+++ scripts/create-mjml-templates.sh
@@ -5,9 +5,11 @@ npx mjml email-templates/verify-email.mjml -o templates/email/verify-email.liqui
 npx mjml email-templates/password-reset.mjml -o templates/email/password-reset.liquid
 npx mjml email-templates/code-v2.mjml -o templates/email/code-v2.liquid
 npx mjml email-templates/link-v2.mjml -o templates/email/link-v2.liquid
+npx mjml email-templates/verify-email.mjml -o templates/email/verify-email.liquid
 
 # also upload to wrangler in this script for now so we can quickly upload new email templates
 npx wrangler r2 object put auth-templates/templates/email/verify-email.liquid -f templates/email/verify-email.liquid
 npx wrangler r2 object put auth-templates/templates/email/password-reset.liquid -f templates/email/password-reset.liquid
 npx wrangler r2 object put auth-templates/templates/email/code-v2.liquid -f templates/email/code-v2.liquid
 npx wrangler r2 object put auth-templates/templates/email/link-v2.liquid -f templates/email/link-v2.liquid
+npx wrangler r2 object put auth-templates/templates/email/verify-email.liquid -f templates/email/verify-email.liquid
