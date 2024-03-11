3b22653b19cf6bb0a85f95b3022a7983769c40a5
Daniel Gent <dan@sesamy.com>
2024-03-11 18:07:28 +0200

    test: unskip test which will fail

--- test/integration/flows/password.spec.ts
+++ test/integration/flows/password.spec.ts
@@ -31,9 +31,9 @@ describe("password-flow", () => {
 
       expect(response.status).toBe(404);
     });
-    // TO FIX - this test will not work now because we require email_validation before logging in...
-    // seems like we need new tests here
-    it.skip("should create a new user with a password and login", async () => {
+    // I think we should move this one to a new describe block
+    // after testing the email validation!
+    it("should create a new user with a password and login", async () => {
       const password = "password";
       const env = await getEnv();
       const client = testClient(tsoaApp, env);
