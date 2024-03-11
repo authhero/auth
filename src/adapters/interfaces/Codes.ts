6381c1ea0232c50a4a855493d24aa34fee290b23
Daniel Gent <dan@sesamy.com>
2024-03-11 16:24:41 +0200

    fix: code list is looking up by user id so make sure param says this

--- src/adapters/interfaces/Codes.ts
+++ src/adapters/interfaces/Codes.ts
@@ -2,5 +2,5 @@ import { Code } from "../../types/Code";
 
 export interface CodesAdapter {
   create: (tenant_id: string, authCode: Code) => Promise<void>;
-  list: (tenant_id: string, email: string) => Promise<Code[]>;
+  list: (tenant_id: string, user_id: string) => Promise<Code[]>;
 }
