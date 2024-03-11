9d3ab1876f80d5bb0b397d70d231705d77bf62e8
Daniel Gent <dan@sesamy.com>
2024-03-11 11:54:18 +0200

    chore: commented out methods never help

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -24,59 +24,6 @@ function getLocale(language: string) {
 
 const engine = new Liquid();
 
-// exportasync function sendEmailValidation(
-//   env: Env,
-//   client: Client,
-//   to: string,
-//   code: string,
-// ) {
-//   if (client.email_validation === "disabled") {
-//     return;
-//   }
-
-//   const response = await env.AUTH_TEMPLATES.get(
-//     "templates/email/verify-email.liquid",
-//   );
-//   if (!response) {
-//     throw new Error("Verify email template not found");
-//   }
-
-//   const templateString = await response.text();
-
-//   const language = client.tenant.language || "sv";
-
-//   const logo = getClientLogoPngGreyBg(
-//     client.tenant.logo ||
-//       "https://assets.sesamy.com/static/images/sesamy/logo-translucent.png",
-//     env.IMAGE_PROXY_URL,
-//   );
-
-//   // TODO - implement i18n
-//   const sendCodeTemplate = engine.parse(templateString);
-//   const codeEmailBody = await engine.render(sendCodeTemplate, {
-//     code,
-//     vendorName: client.name,
-//     logo,
-//   });
-
-//   await sendEmail(client, {
-//     to: [{ email: to, name: to }],
-//     from: {
-//       email: client.tenant.sender_email,
-//       name: client.tenant.sender_name,
-//     },
-//     content: [
-//       {
-//         type: "text/html",
-//         value: codeEmailBody,
-//       },
-//     ],
-//     subject: translate(language, "codeEmailTitle")
-//       .replace("{{vendorName}}", client.name)
-//       .replace("{{code}}", code),
-//   });
-// }
-
 export async function sendCode(
   env: Env,
   client: Client,
