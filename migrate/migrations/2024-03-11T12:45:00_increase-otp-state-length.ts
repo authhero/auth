299550724af227d617f08f1aa707a8f5ce7e67ca
Markus Ahlstrand <markus@ahlstrand.de>
2024-03-12 11:38:46 +0100

    fix: remove migration changes

--- migrate/migrations/2024-03-11T12:45:00_increase-otp-state-length.ts
+++ migrate/migrations/2024-03-11T12:45:00_increase-otp-state-length.ts
@@ -3,18 +3,18 @@ import { Database } from "../../src/types";
 
 export async function up(db: Kysely<Database>): Promise<void> {
   // Uncomment this for planetscale migration
-  await db.schema
-    .alterTable("otps")
-    // column this long working on planetscale on logs column
-    .modifyColumn("state", "varchar(8192)")
-    .execute();
+  //   await db.schema
+  //     .alterTable("otps")
+  //     // column this long working on planetscale on logs column
+  //     .modifyColumn("state", "varchar(8192)")
+  //     .execute();
 
   // In SQLite we have to drop and recreate the column
-  // await db.schema.alterTable("otps").dropColumn("state").execute();
-  // await db.schema
-  //   .alterTable("otps")
-  //   .addColumn("state", "varchar(8192)")
-  //   .execute();
+  await db.schema.alterTable("otps").dropColumn("state").execute();
+  await db.schema
+    .alterTable("otps")
+    .addColumn("state", "varchar(8192)")
+    .execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
