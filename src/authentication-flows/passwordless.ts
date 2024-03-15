9452148cb9ec13eb4602e1385c1721a3b7b3592f
Daniel Gent <dan@sesamy.com>
2024-03-15 13:34:24 +0200

    feat: extract out common send email verification email

--- src/authentication-flows/passwordless.ts
+++ src/authentication-flows/passwordless.ts
@@ -6,7 +6,14 @@ import {
   getPrimaryUserByEmailAndProvider,
   getPrimaryUserByEmail,
 } from "../utils/users";
-import { User } from "../types";
+import { User, Client, AuthParams } from "../types";
+import { UniversalLoginSession } from "../adapters/interfaces/UniversalLoginSession";
+import { nanoid } from "nanoid";
+import generateOTP from "../utils/otp";
+import { UNIVERSAL_AUTH_SESSION_EXPIRES_IN_SECONDS } from "../constants";
+
+// de-dupe
+const CODE_EXPIRATION_TIME = 24 * 60 * 60 * 1000;
 
 interface LoginParams {
   client_id: string;
@@ -64,4 +71,62 @@ export async function validateCode(
   });
 
   return primaryUser || newUser;
+}
+
+// this is not inside src/controllers/email/sendValidateEmailAddress
+//  because we're mocking all that for the tests!
+// We probably shouldn't do this and instead only mock the lowest level sendEmail function
+// but then -> we don't have access to the templates in the bun tests...
+// can we mock templates? or even properly use them?
+
+interface sendEmailVerificationEmailParams {
+  env: Env;
+  client: Client;
+  user: User;
+}
+
+export async function sendEmailVerificationEmail({
+  env,
+  client,
+  user,
+}: sendEmailVerificationEmailParams) {
+  const authParams: AuthParams = {
+    client_id: client.id,
+    username: user.email,
+  };
+
+  const session: UniversalLoginSession = {
+    id: nanoid(),
+    client_id: client.id,
+    tenant_id: client.tenant_id,
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+    expires_at: new Date(
+      Date.now() + UNIVERSAL_AUTH_SESSION_EXPIRES_IN_SECONDS * 1000,
+    ).toISOString(),
+    authParams,
+  };
+
+  await env.data.universalLoginSessions.create(session);
+
+  const state = session.id;
+
+  const code = generateOTP();
+
+  await env.data.codes.create(client.tenant_id, {
+    id: nanoid(),
+    code,
+    type: "validation",
+    user_id: user.id,
+    created_at: new Date().toISOString(),
+    expires_at: new Date(Date.now() + CODE_EXPIRATION_TIME).toISOString(),
+  });
+
+  await env.data.email.sendValidateEmailAddress(
+    env,
+    client,
+    user.email,
+    code,
+    state,
+  );
 }
