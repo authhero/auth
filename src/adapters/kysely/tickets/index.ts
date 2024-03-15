31a0dc51b24806089945d47a3a18c5a9b92ccab7
Daniel Gent <dan@sesamy.com>
2024-03-15 14:38:08 +0200

    feat: remove tickets adapter

--- src/adapters/kysely/tickets/index.ts
+++ src/adapters/kysely/tickets/index.ts
@@ -2,11 +2,13 @@ import { TicketsAdapter } from "../../interfaces/Tickets";
 import { Database } from "../../../types";
 import { get } from "./get";
 import { create } from "./create";
+import { remove } from "./remove";
 import { Kysely } from "kysely";
 
 export function createTicketsAdapter(db: Kysely<Database>): TicketsAdapter {
   return {
     create: create(db),
     get: get(db),
+    remove: remove(db),
   };
 }
---
+++ src/adapters/kysely/tickets/remove.ts
@@ -0,0 +1,15 @@
+import { Database, OTP } from "../../../types";
+import { Kysely } from "kysely";
+
+export function remove(db: Kysely<Database>) {
+  return async (tenant_id: string, id: string) => {
+    await db
+      .updateTable("tickets")
+      .set({
+        used_at: new Date().toISOString(),
+      })
+      .where("tickets.tenant_id", "=", tenant_id)
+      .where("tickets.id", "=", id)
+      .execute();
+  };
+}
