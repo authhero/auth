9ca591ea239521b3baaaa4c4acfc039cc88cbc0f
Daniel Gent <dan@sesamy.com>
2024-03-18 13:32:40 +0200

    chore; few different attempts for JSX to work, all end in failure

--- src/utils/reactdemo.tsx
+++ src/utils/reactdemo.tsx
@@ -2,6 +2,8 @@ import type { FC } from "hono/jsx";
 import { Context } from "hono";
 import { Var } from "../types/Var";
 import { Env } from "../types/Env";
+// import {html} from "hono/jsx";
+import { html } from "hono/html";
 
 const ReactThing: FC<{}> = (props: {}) => {
   return (
@@ -18,8 +20,14 @@ export async function renderReactThing(
   ctx: Context<{ Bindings: Env; Variables: Var }>,
 ) {
   // cannot do this! need to return string
-  return ctx.html(<p>Hello world</p>);
+  // return ctx.html(<p>Hello world</p>);
+
+  // this just renders a string in quotes
+  // return html`<p>Hello world</p>`;
 
   // try using actual react components
-  //   return ctx.html(<ReactThing />);
+  // return ctx.html(<ReactThing />);
+
+  // just renders a string. no knowledge of HTML
+  return html`<ReactThing />`;
 }
