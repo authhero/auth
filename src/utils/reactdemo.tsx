31c884006857d94a5575bc36eaefa5ae25ba5b6e
Daniel Gent <dan@sesamy.com>
2024-03-18 14:00:04 +0200

    feat: use this layout

--- src/utils/reactdemo.tsx
+++ src/utils/reactdemo.tsx
@@ -50,40 +50,37 @@ const Layout: FC<{ title: string }> = ({ title, children }) => {
 
 const ResetPasswordPage: FC<{}> = (props: {}) => {
   return (
-    <html>
-      <body>
-        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-          <form class="login100-form validate-form" method="post">
-            <span class="login100-form-title p-b-49">Change password</span>
-            <div class="wrap-input100 validate-input m-b-23">
-              <span class="label-input100">
-                New password - at some point we'll add a second confirmation one
-                AND we need to tell the user it has to have whatever rules we
-                specify... OR we just echo back error messages from auth2 in
-                HTML
-              </span>
-              <input
-                class="input100"
-                type="text"
-                name="password"
-                placeholder="Type your password"
-                value="{{password}}"
-                required
-              />
-              <span class="focus-input100" data-symbol=""></span>
+    <Layout title="Reset Password">
+      <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+        <form class="login100-form validate-form" method="post">
+          <span class="login100-form-title p-b-49">Change password</span>
+          <div class="wrap-input100 validate-input m-b-23">
+            <span class="label-input100">
+              New password - at some point we'll add a second confirmation one
+              AND we need to tell the user it has to have whatever rules we
+              specify... OR we just echo back error messages from auth2 in HTML
+            </span>
+            <input
+              class="input100"
+              type="text"
+              name="password"
+              placeholder="Type your password"
+              value="{{password}}"
+              required
+            />
+            <span class="focus-input100" data-symbol=""></span>
+          </div>
+          {/* TODO */}
+          {/* <div>{{ errorMessage }}</div> */}
+          <div class="container-login100-form-btn">
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button class="login100-form-btn">Change password</button>
             </div>
-            {/* TODO */}
-            {/* <div>{{ errorMessage }}</div> */}
-            <div class="container-login100-form-btn">
-              <div class="wrap-login100-form-btn">
-                <div class="login100-form-bgbtn"></div>
-                <button class="login100-form-btn">Change password</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </body>
-    </html>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 };
 
