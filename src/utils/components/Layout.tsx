37a4a3173794213d58f6c7f14b155433365c20da
Daniel Gent <dan@sesamy.com>
2024-03-18 15:33:28 +0200

    feat: clone Layout from login2

--- src/utils/components/Layout.tsx
+++ src/utils/components/Layout.tsx
@@ -1,7 +1,4 @@
 import type { FC } from "hono/jsx";
-import { Context } from "hono";
-import { Var } from "../../types/Var";
-import { Env } from "../../types/Env";
 
 const Layout: FC<{ title: string }> = ({ title, children }) => {
   return (
@@ -37,11 +34,34 @@ const Layout: FC<{ title: string }> = ({ title, children }) => {
       </head>
 
       <body>
-        <div
-          class="container-login100"
-          style="background-image: url('/static/images/bg-01.webp');"
-        >
-          {children}
+        <div className="row min-h-full w-full overflow-hidden bg-cover bg-center text-sm sm:bg-fixed sm:bg-left-top">
+          <div className="row-up-left w-[calc(100%-theme(space.2)-theme(space.2))] max-w-[1295px] !flex-nowrap sm:w-[calc(100%-theme(space.16)-theme(space.16))]">
+            <div className="column-left w-full sm:w-auto">
+              <div className="relative flex w-full flex-col rounded-2xl bg-white px-5 py-10 dark:bg-gray-800 dark:text-white sm:min-h-[700px] sm:max-w-md sm:px-14 sm:py-14 md:min-w-[448px] short:min-h-[558px] min-h-[calc(100vh-83px)]">
+                {/* <div className="mb-16">
+                    <AppLogo />
+                  </div> */}
+                <div className="flex flex-1 flex-col">
+                  {children}
+                  {/* <Footer vendorSettings={vendorSettings} /> */}
+                </div>
+              </div>
+
+              <div className="flex w-full items-center px-6 pb-8 pt-4 justify-between">
+                <div className="flex justify-center space-x-2 text-xs text-white sm:justify-normal md:text-xs">
+                  <a
+                    className="text-xs text-white hover:underline md:text-xs"
+                    // href={supportUrl}
+                    href="https://sesamy.com"
+                  >
+                    {t("contact_support")}
+                  </a>
+                  <span className="text-gray-300">|</span>{" "}
+                  <span>copyright_sesamy</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </body>
     </html>
