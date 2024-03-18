fae712e954f9da90598420c24a1c6efed75765a7
Daniel Gent <dan@sesamy.com>
2024-03-18 15:29:58 +0200

    feat: move Layout into own component

--- src/utils/reactdemo.tsx
+++ src/utils/reactdemo.tsx
@@ -2,51 +2,7 @@ import type { FC } from "hono/jsx";
 import { Context } from "hono";
 import { Var } from "../types/Var";
 import { Env } from "../types/Env";
-
-const Layout: FC<{ title: string }> = ({ title, children }) => {
-  return (
-    <html lang="en">
-      <head>
-        <title>{title}</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta name="robots" content="noindex, follow" />
-
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/stylesheets/font-awesome.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/stylesheets/material-design-iconic-font.min.css"
-        />
-
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/stylesheets/main.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/stylesheets/util.css"
-        />
-      </head>
-
-      <body>
-        <div
-          class="container-login100"
-          style="background-image: url('/static/images/bg-01.webp');"
-        >
-          {children}
-        </div>
-      </body>
-    </html>
-  );
-};
+import Layout from "./components/Layout";
 
 const ResetPasswordPage: FC<{}> = (props: {}) => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   darkMode: "class",
   content: [
     // wilcard from docs
-    "./src/**/*.{html,js}",
+    "./src/**/*.tsx",
   ],
   theme: {
     extend: {
