78eb93f350c2d5f4530607394d79f84def07207e
Daniel Gent <dan@sesamy.com>
2024-03-18 14:26:31 +0200

    chore: wildcard tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    // wilcard from docs
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {
