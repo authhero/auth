4b39a0bf80e8719e6d69f5c9d5e6bbf7ddab28d7
Daniel Gent <dan@sesamy.com>
2024-03-20 13:10:47 +0200

    test: comprehensive tests for each password strength rule

--- test/utils/validatePassword.spec.ts
+++ test/utils/validatePassword.spec.ts
@@ -1,14 +1,38 @@
 import validatePassword from "../../src/utils/validatePassword";
 
 describe("validatePassword", () => {
-  it("should return true if the password is strong", () => {
+  it("should return true if the password matches all the conditions", () => {
     const password = "Password123!";
     const result = validatePassword(password);
     expect(result).toBe(true);
   });
 
-  it("should return false if the password is weak", () => {
-    const password = "password";
+  it("should return false if the password does not contain a special character", () => {
+    const password = "Password123";
+    const result = validatePassword(password);
+    expect(result).toBe(false);
+  });
+
+  it("should return false if the password does not contain a lower case character", () => {
+    const password = "PASSWORD123!";
+    const result = validatePassword(password);
+    expect(result).toBe(false);
+  });
+
+  it("should return false if the password does not contain an upper case character", () => {
+    const password = "password123!";
+    const result = validatePassword(password);
+    expect(result).toBe(false);
+  });
+
+  it("should return false if the password does not contain a number", () => {
+    const password = "Password!";
+    const result = validatePassword(password);
+    expect(result).toBe(false);
+  });
+
+  it("should return false if the password is less than 8 characters", () => {
+    const password = "Pass1!";
     const result = validatePassword(password);
     expect(result).toBe(false);
   });
