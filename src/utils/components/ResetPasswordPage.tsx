4bf274ea71f3e121267cae9890b04309af963fdb
Daniel Gent <dan@sesamy.com>
2024-03-21 12:21:15 +0200

    feat: password confirmation input

--- src/utils/components/ResetPasswordPage.tsx
+++ src/utils/components/ResetPasswordPage.tsx
@@ -14,6 +14,18 @@ const ResetPasswordPage: FC<{ error?: string }> = ({ error }) => {
             placeholder="enter new password"
             class="mb-2 w-full rounded-lg bg-gray-100 px-4 py-5 text-base placeholder:text-gray-300 dark:bg-gray-600 md:text-base"
           />
+          {/*
+            this page is not following Auth0's naming. just this field
+            auth0 does password matching serverside
+            BUT password strength is done clientside
+            ALSO the inputs have the eye-button toggle to show the password
+          */}
+          <input
+            type="text"
+            name="re-enter-password"
+            placeholder="confirm password"
+            class="mb-2 w-full rounded-lg bg-gray-100 px-4 py-5 text-base placeholder:text-gray-300 dark:bg-gray-600 md:text-base"
+          />
           {error && <em class="mb-2 bg-red">{error}</em>}
           <Button>Change password</Button>
         </form>
