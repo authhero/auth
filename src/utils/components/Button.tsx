65aeceb09729ac9926694c74c0da66b8e3ab6c11
Daniel Gent <dan@sesamy.com>
2024-03-25 16:25:33 +0200

    chore: add classnames dep

--- src/utils/components/Button.tsx
+++ src/utils/components/Button.tsx
@@ -1,5 +1,31 @@
 import type { FC } from "hono/jsx";
 
+/*
+need to properly set all these
+
+className={cn(
+          "relative w-full rounded-lg text-center",
+          {
+            "px-4 py-5": variant !== "custom",
+            "text-white": bgColor !== "primary" && textColor === "white",
+            "text-black": bgColor !== "primary" && textColor === "black",
+            "bg-primary text-textOnPrimary hover:bg-primaryHover":
+              variant === "primary" && bgColor === "primary",
+            "bg-[#1876D2] hover:bg-[#1876D2]/90":
+              variant === "primary" && bgColor === "blue",
+            "bg-[#EA4336] hover:bg-[#EA4336]/90":
+              variant === "primary" && bgColor === "red",
+            "bg-black hover:bg-black/90":
+              variant === "primary" && bgColor === "black",
+            "border border-gray-300 bg-white text-black":
+              variant === "secondary",
+            "pointer-events-none cursor-not-allowed opacity-40": disabled,
+          },
+          className,
+        )}
+
+        */
+
 const Button: FC<{}> = ({ children }) => {
   return (
     <button
