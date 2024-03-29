4eeba0e6bcc1e64c354f6436efde95bbad01efaa
Daniel Gent <dan@sesamy.com>
2024-03-29 16:00:05 +0200

    fix: lol className thanks react

--- src/utils/components/Footer.tsx
+++ src/utils/components/Footer.tsx
@@ -9,15 +9,15 @@ const Footer = ({ vendorSettings }: Props) => {
   const { termsAndConditionsUrl } = vendorSettings || {};
 
   return (
-    <div className="mt-8">
-      <div className="text-xs text-gray-300">
+    <div class="mt-8">
+      <div class="text-xs text-gray-300">
         {i18next.t("agree_to")}{" "}
         <a
           href={
             termsAndConditionsUrl ||
             "https://store.sesamy.com/pages/terms-of-service"
           }
-          className="text-primary hover:underline"
+          class="text-primary hover:underline"
           target="_blank"
         >
           {i18next.t("terms")}
