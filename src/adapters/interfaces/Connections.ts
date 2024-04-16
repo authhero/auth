d35cc7178ea39715a66937f858f34778e750e86f
Markus Ahlstrand <markus@ahlstrand.de>
2024-04-16 23:23:45 +0200

    fix: list connections

--- src/adapters/interfaces/Connections.ts
+++ src/adapters/interfaces/Connections.ts
@@ -1,7 +1,12 @@
+import { Totals } from "../../types";
 import { Connection, ConnectionInsert } from "../../types/Connection";
 import { ListParams } from "./ListParams";
 
+export interface ListConnectionsResponse extends Totals {
+  connections: Connection[];
+}
+
 export interface ConnectionsAdapter {
   create(tenant_id: string, params: ConnectionInsert): Promise<Connection>;
-  list(tenant_id: string, params: ListParams): Promise<Connection[]>;
+  list(tenant_id: string, params: ListParams): Promise<ListConnectionsResponse>;
 }
--- src/adapters/kysely/connections/list.ts
+++ src/adapters/kysely/connections/list.ts
@@ -1,18 +1,40 @@
-import { z } from "zod";
 import { Database } from "../../../types";
-import { connectionSchema } from "../../../types/Connection";
 import { Kysely } from "kysely";
+import { ListParams } from "../../../adapters/interfaces/ListParams";
+import { luceneFilter } from "../helpers/filter";
+import getCountAsInt from "../../../utils/getCountAsInt";
+import { ListConnectionsResponse } from "../../../adapters/interfaces/Connections";
 
 export function list(db: Kysely<Database>) {
-  return async (tenantId: string) => {
+  return async (
+    tenantId: string,
+    params: ListParams,
+  ): Promise<ListConnectionsResponse> => {
     let query = db
       .selectFrom("connections")
       .where("connections.tenant_id", "=", tenantId);
 
-    const connections = z
-      .array(connectionSchema)
-      .parse(await query.selectAll().execute());
+    if (params.q) {
+      query = luceneFilter(db, query, params.q, ["user_id", "ip"]);
+    }
 
-    return connections;
+    const filteredQuery = query
+      .offset(params.page * params.per_page)
+      .limit(params.per_page);
+
+    const connections = await filteredQuery.selectAll().execute();
+
+    const [{ count }] = await query
+      .select((eb) => eb.fn.countAll().as("count"))
+      .execute();
+
+    const countInt = getCountAsInt(count);
+
+    return {
+      connections,
+      start: params.page * params.per_page,
+      limit: params.per_page,
+      length: countInt,
+    };
   };
 }
