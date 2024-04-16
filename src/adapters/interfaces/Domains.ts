80ace5ca6ea0852b38840ec47c727bcf17c538bc
Markus Ahlstrand <markus@ahlstrand.de>
2024-04-16 19:36:35 +0200

    fix: domain list result

--- src/adapters/interfaces/Domains.ts
+++ src/adapters/interfaces/Domains.ts
@@ -2,10 +2,11 @@ import { Domain } from "../../types/Domain";
 import { Totals } from "../../types";
 import { ListParams } from "./ListParams";
 
+export interface ListDomainsResponse extends Totals {
+  domains: Domain[];
+}
+
 export interface DomainsAdapter {
   create(tenant_id: string, params: Domain): Promise<Domain>;
-  list(
-    tenant_id: string,
-    params: ListParams,
-  ): Promise<{ domains: Domain[]; totals?: Totals }>;
+  list(tenant_id: string, params: ListParams): Promise<ListDomainsResponse>;
 }
