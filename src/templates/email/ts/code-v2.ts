314d5c376f543bb916cfab9e9c355651871ff8e0
Daniel Gent <dan@sesamy.com>
2024-04-22 16:33:04 +0300

    feat: commit build output

--- src/templates/email/ts/code-v2.ts
+++ src/templates/email/ts/code-v2.ts
@@ -1,5 +1,5 @@
-export const codev2 = `
-<!doctype html>
+export const codeV2 = `
+    <!doctype html>
 <html lang="und" dir="auto" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
--- src/templates/email/ts/link-v2.ts
+++ src/templates/email/ts/link-v2.ts
@@ -1,5 +1,5 @@
-export const linkv2 = `
-<!doctype html>
+export const linkV2 = `
+    <!doctype html>
 <html lang="und" dir="auto" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
