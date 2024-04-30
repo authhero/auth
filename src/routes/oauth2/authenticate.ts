9318aa9eb5795144f664d7ee3f8c2c5aa9f8db9d
Daniel Gent <dan@sesamy.com>
2024-04-30 11:29:52 +0100

    fix: only support JSON

--- src/routes/oauth2/authenticate.ts
+++ src/routes/oauth2/authenticate.ts
@@ -20,19 +20,20 @@ export const authenticateRoutes = new OpenAPIHono<{ Bindings: Env }>()
       request: {
         body: {
           content: {
-            "application/x-www-form-urlencoded": {
-              schema: z.object({
-                credential_type: z.enum([
-                  "http://auth0.com/oauth/grant-type/passwordless/otp",
-                  "http://auth0.com/oauth/grant-type/password-realm",
-                ]),
-                otp: z.string().optional(),
-                client_id: z.string(),
-                username: z.string().transform((v) => v.toLowerCase()),
-                password: z.string().optional(),
-                realm: z.enum(["email", "Username-Password-Authentication"]),
-              }),
-            },
+            // no idea how to support both, and do we need to?
+            // "application/x-www-form-urlencoded": {
+            //   schema: z.object({
+            //     credential_type: z.enum([
+            //       "http://auth0.com/oauth/grant-type/passwordless/otp",
+            //       "http://auth0.com/oauth/grant-type/password-realm",
+            //     ]),
+            //     otp: z.string().optional(),
+            //     client_id: z.string(),
+            //     username: z.string().transform((v) => v.toLowerCase()),
+            //     password: z.string().optional(),
+            //     realm: z.enum(["email", "Username-Password-Authentication"]),
+            //   }),
+            // },
             "application/json": {
               schema: z.object({
                 credential_type: z.enum([
@@ -56,7 +57,7 @@ export const authenticateRoutes = new OpenAPIHono<{ Bindings: Env }>()
       },
     }),
     async (ctx) => {
-      const { client_id, username, otp, password } = ctx.req.valid("form");
+      const { client_id, username, otp, password } = ctx.req.valid("json");
       const client = await getClient(ctx.env, client_id);
 
       if (!client) {
