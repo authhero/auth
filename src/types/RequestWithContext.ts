29c6cd79b3f8b72cb650d20494141b8320641d4a
Daniel Gent <dan@sesamy.com>
2024-05-02 13:29:22 +0100

    fix: another round unused files

--- src/types/RequestWithContext.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "hono";
-import { Env } from ".";
-import { Var } from "./Var";
-
-export interface RequestWithContext extends Request {
-  ctx: Context<{ Bindings: Env; Variables: Var }>;
-}
