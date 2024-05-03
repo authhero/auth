27ef12a2174e3e3b61b450fb67b4f6e9a05d632b
Daniel Gent <dan@sesamy.com>
2024-05-03 13:44:42 +0100

    feat: use forgotpassword jsx

--- src/templates/render.ts
+++ src/templates/render.ts
@@ -1,24 +1,8 @@
 import { Liquid } from "liquidjs";
 import { UniversalLoginSession } from "../adapters/interfaces/UniversalLoginSession";
-import { layout, forgotPassword, message } from "./universal";
+import { layout, message } from "./universal";
 
 const engine = new Liquid();
-
-export async function renderForgotPassword(
-  context: UniversalLoginSession,
-  state: string,
-): Promise<string> {
-  const layoutTemplate = engine.parse(layout);
-
-  const template = engine.parse(forgotPassword);
-
-  const content = await engine.render(template, { ...context, state });
-  return engine.render(layoutTemplate, {
-    ...context,
-    state, // seems inconsistent doing this here... does it need doing in both places?
-    content,
-  });
-}
 
 export async function renderMessage(
   context: UniversalLoginSession | { page_title: string; message: string },
