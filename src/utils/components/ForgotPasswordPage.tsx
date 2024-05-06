1349239147f59d02f11c4301fec30c9154e2ef59
Daniel Gent <dan@sesamy.com>
2024-05-06 18:50:44 +0100

    fix: forgot password i18n

--- src/utils/components/ForgotPasswordPage.tsx
+++ src/utils/components/ForgotPasswordPage.tsx
@@ -2,6 +2,7 @@ import type { FC } from "hono/jsx";
 import Layout from "./Layout";
 import Button from "./Button";
 import { VendorSettings } from "../../types";
+import i18next from "i18next";
 
 type Props = {
   error?: string;
@@ -11,18 +12,22 @@ type Props = {
 const ForgotPasswordPage: FC<Props> = ({ error, vendorSettings }) => {
   return (
     <Layout title="Login" vendorSettings={vendorSettings}>
-      <div class="mb-4 text-lg font-medium sm:text-2xl">Login</div>
-      <div class="mb-6 text-gray-300">Forgot password?</div>
+      <div class="mb-4 text-lg font-medium sm:text-2xl">
+        {i18next.t("forgot_password_title")}
+      </div>
+      <div class="mb-6 text-gray-300">
+        {i18next.t("forgot_password_description")}
+      </div>
       <div class="flex flex-1 flex-col justify-center">
         <form method="post">
           <input
             type="text"
             name="email"
-            placeholder="email"
+            placeholder={i18next.t("forgot_password_email_placeholder")}
             class="mb-2 w-full rounded-lg bg-gray-100 px-4 py-5 text-base placeholder:text-gray-300 dark:bg-gray-600 md:text-base"
           />
           {error && <em class="mb-2 bg-red">{error}</em>}
-          <Button>Continue</Button>
+          <Button>{i18next.t("forgot_password_cta")}</Button>
         </form>
       </div>
     </Layout>
