427a0787ae772e493c2edc25003391fe5af71c7b
Daniel Gent <dan@sesamy.com>
2024-05-07 17:40:38 +0100

    feat: use i18n on enter email

--- src/utils/components/LoginWithCodePage.tsx
+++ src/utils/components/LoginWithCodePage.tsx
@@ -2,27 +2,39 @@ import type { FC } from "hono/jsx";
 import Layout from "./Layout";
 import Button from "./Button";
 import { VendorSettings } from "../../types";
+import i18next from "i18next";
 
 type Props = {
   error?: string;
   vendorSettings: VendorSettings;
 };
 
+// this page is called enter-email on login2... maybe we should copy those page names
 const LoginWithCodePage: FC<Props> = ({ error, vendorSettings }) => {
+  // TODO - get this from auth0header
+  const sendType = "code";
+
+  const loginDescriptionText =
+    sendType === "code"
+      ? i18next.t("login_description_code")
+      : i18next.t("login_description_link");
+
   return (
     <Layout title="Login with Code" vendorSettings={vendorSettings}>
-      <div class="mb-4 text-lg font-medium sm:text-2xl">Login</div>
-      <div class="mb-6 text-gray-300">Enter your email to receive a code</div>
+      <div class="mb-4 text-lg font-medium sm:text-2xl">
+        {i18next.t("welcome")}
+      </div>
+      <div class="mb-6 text-gray-300">{loginDescriptionText}</div>
       <div class="flex flex-1 flex-col justify-center">
         <form method="post">
           <input
             type="text"
             name="username"
-            placeholder="email"
+            placeholder={i18next.t("email_placeholder")}
             class="mb-2 w-full rounded-lg bg-gray-100 px-4 py-5 text-base placeholder:text-gray-300 dark:bg-gray-600 md:text-base"
           />
           {error && <em class="mb-2 bg-red">{error}</em>}
-          <Button>Send code</Button>
+          <Button>{i18next.t("continue")}</Button>
         </form>
       </div>
     </Layout>
