3c1d5d6bfd4b0529d9cafaff36a1cd1b3278de9e
Daniel Gent <dan@sesamy.com>
2024-05-16 17:01:55 +0100

    test: snapshot fallback to sesamy vendor

---
+++ test/integration/login/vendor-settings.spec.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect } from "vitest";
+import { getEnv } from "../helpers/test-client";
+import { oauthApp } from "../../../src/app";
+import { testClient } from "hono/testing";
+import { snapshotResponse } from "../helpers/playwrightSnapshots";
+import { AuthorizationResponseType } from "../../../src/types";
+
+describe("Vendor settings", () => {
+  it("should fallback to sesamy styling with invalid vendor_id", async () => {
+    const env = await getEnv({
+      testTenantLanguage: "nb",
+    });
+    const oauthClient = testClient(oauthApp, env);
+
+    const response = await oauthClient.authorize.$get({
+      query: {
+        client_id: "clientId",
+        // non-existing vendor!
+        vendor_id: "non-existent-vendor",
+        response_type: AuthorizationResponseType.TOKEN_ID_TOKEN,
+        scope: "openid",
+        redirect_uri: "http://localhost:3000/callback",
+        state: "state",
+      },
+    });
+    const location = response.headers.get("location");
+    const stateParam = new URLSearchParams(location!.split("?")[1]);
+
+    const query = Object.fromEntries(stateParam.entries());
+
+    const codeInputFormResponse = await oauthClient.u.code.$get({
+      query: {
+        state: query.state,
+      },
+    });
+
+    expect(codeInputFormResponse.status).toBe(200);
+
+    await snapshotResponse(codeInputFormResponse);
+  });
+});
