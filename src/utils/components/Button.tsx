2f45accb7c76c2260fd29819b9c1e2399aa9e41e
Daniel Gent <dan@sesamy.com>
2024-05-17 11:46:55 +0100

    feat: button variant

--- src/utils/components/Button.tsx
+++ src/utils/components/Button.tsx
@@ -5,15 +5,22 @@ type Props = {
   children: (string | JSX.Element)[] | string | JSX.Element;
   className?: string;
   Component?: string;
+  variant?: "primary" | "secondary";
 };
 
-const Button: FC<Props> = ({ children, className, Component = "button" }) => {
+const Button: FC<Props> = ({
+  children,
+  className,
+  Component = "button",
+  variant = "primary",
+}) => {
   return (
     <Component
-      class={cn(
-        "relative w-full rounded-lg text-center px-4 py-5 bg-primary text-textOnPrimary hover:bg-primaryHover",
-        className,
-      )}
+      class={cn("relative w-full rounded-lg text-center px-4 py-5", className, {
+        "bg-primary text-textOnPrimary hover:bg-primaryHover":
+          variant === "primary",
+        "border border-gray-300 bg-white text-black": variant === "secondary",
+      })}
       type="submit"
     >
       <span class="flex items-center justify-center space-x-2">{children}</span>
