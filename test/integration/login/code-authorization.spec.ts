0f37b1b82e6a866154f1e2b71f16df2dcc2f760e
Daniel Gent <dan@sesamy.com>
2024-05-27 18:39:54 +0100

    feat: test authorization code flow

--- test/integration/login/code-authorization.spec.ts
+++ test/integration/login/code-authorization.spec.ts
@@ -3,6 +3,7 @@ import { getEnv } from "../helpers/test-client";
 import { oauthApp } from "../../../src/app";
 import { testClient } from "hono/testing";
 import { AuthorizationResponseType } from "../../../src/types";
+import { parseJwt } from "../../../src/utils/parse-jwt";
 
 test("code authorization flow should work", async () => {
   const env = await getEnv({
@@ -51,5 +52,42 @@ test("code authorization flow should work", async () => {
 
   const code = redirectUrl.searchParams.get("code");
 
-  console.log(code);
+  // --------------------------------
+  // exchange code for token
+  // --------------------------------
+
+  const tokenResponse = await oauthClient.oauth.token.$post({
+    form: {
+      grant_type: "authorization_code",
+      code: code!,
+      client_id: "clientId",
+      // this is the client_secret in DEFAULT_CLIENT
+      client_secret: "secret",
+      redirect_uri: "http://localhost:3000/callback",
+    },
+  });
+
+  expect(tokenResponse.status).toBe(200);
+
+  const tokens = (await tokenResponse.json()) as {
+    access_token: string;
+    id_token: string;
+    token_type: string;
+    expires_in: number;
+  };
+
+  const accessTokenPayload = parseJwt(tokens.access_token!);
+
+  expect(accessTokenPayload.sub).toEqual("auth2|userId");
+
+  const idTokenPayload = parseJwt(tokens.id_token!);
+
+  expect(idTokenPayload.sub).toEqual("auth2|userId");
+
+  // TO TEST? more fields on tokens?
 });
+
+// TO TEST
+// invalid secret?
+// invalid code?
+// invalid client_id?
