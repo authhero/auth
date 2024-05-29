974c87187f59ca6526e1a0c874c3aafc7167268e
Daniel Gent <dan@sesamy.com>
2024-05-29 13:52:10 +0100

    fix: email validation message

--- src/routes/tsx/routes.tsx
+++ src/routes/tsx/routes.tsx
@@ -1137,7 +1137,10 @@ export const loginRoutes = new OpenAPIHono<{ Bindings: Env }>()
 
       const { env } = ctx;
 
-      const { client, session } = await initJSXRoute(state, env);
+      const { client, session, vendorSettings } = await initJSXRoute(
+        state,
+        env,
+      );
 
       const email = session.authParams.username;
       if (!email) {
@@ -1199,8 +1202,13 @@ export const loginRoutes = new OpenAPIHono<{ Bindings: Env }>()
         }
       }
 
-      // what should we actually do here?
-      return ctx.text("email validated");
+      return ctx.html(
+        <MessagePage
+          message={i18next.t("email_validated")}
+          pageTitle={i18next.t("email_validated")}
+          vendorSettings={vendorSettings}
+        />,
+      );
     },
   )
 
