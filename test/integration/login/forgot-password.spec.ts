1acf813da086d539d6d416686333c1d268d36544
Daniel Gent <dan@sesamy.com>
2024-05-31 15:19:20 +0100

    test: should not send email for non-existing email

--- test/integration/login/forgot-password.spec.ts
+++ test/integration/login/forgot-password.spec.ts
@@ -8,6 +8,14 @@ import {
 } from "../helpers/playwrightSnapshots";
 import { AuthorizationResponseType } from "../../../src/types";
 
+const DEFAULT_AUTHORIZE_PARAMS = {
+  client_id: "clientId",
+  response_type: AuthorizationResponseType.TOKEN_ID_TOKEN,
+  scope: "openid",
+  redirect_uri: "http://localhost:3000/callback",
+  state: "state",
+};
+
 describe("Forgot password", () => {
   it("should send forgot password email", async () => {
     const env = await getEnv();
@@ -15,13 +23,7 @@ describe("Forgot password", () => {
     const oauthClient = testClient(oauthApp, env);
 
     const response = await oauthClient.authorize.$get({
-      query: {
-        client_id: "clientId",
-        response_type: AuthorizationResponseType.TOKEN_ID_TOKEN,
-        scope: "openid",
-        redirect_uri: "http://localhost:3000/callback",
-        state: "state",
-      },
+      query: DEFAULT_AUTHORIZE_PARAMS,
     });
 
     const location = response.headers.get("location");
@@ -54,6 +56,7 @@ describe("Forgot password", () => {
         state: query.state,
       },
       form: {
+        // this user exists in the fixutres
         username: "foo@example.com",
       },
     });
@@ -73,7 +76,47 @@ describe("Forgot password", () => {
     // we are testing that flow in test/integration/flows/password.spec.ts
     // BUT this email is sent from dbconnections.change_password e.g. the auth0.js method
   });
+
+  it("should not send a forgot password email for a non-existing email address", async () => {
+    const env = await getEnv();
+
+    const oauthClient = testClient(oauthApp, env);
+
+    const response = await oauthClient.authorize.$get({
+      query: DEFAULT_AUTHORIZE_PARAMS,
+    });
+
+    const location = response.headers.get("location");
+
+    const stateParam = new URLSearchParams(location!.split("?")[1]);
+    const query = Object.fromEntries(stateParam.entries());
+
+    // ---------------------
+    // try and send a password reset email to non-existing user
+    // ---------------------
+
+    const forgotPasswordEmailResponse = await oauthClient.u[
+      "forgot-password"
+    ].$post({
+      query: {
+        state: query.state,
+      },
+      form: {
+        username: "this-user-does-not-exist@example.com",
+      },
+    });
+
+    expect(forgotPasswordEmailResponse.status).toBe(200);
+
+    await snapshotResponse(forgotPasswordEmailResponse);
+
+    // ---------------------
+    // no email has been sent though
+    // ---------------------
+
+    expect(env.data.emails).toHaveLength(0);
+  });
 });
 
 // TO TEST
-// what happens when send to a non-existent password user? maybe the user exists as an email user
+// what happens when send to a non-existent password user but the user exists as an email user
