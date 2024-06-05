a64cb6da891a30ef61e35c0e1477b5e0fee00b06
Daniel Gent <dan@sesamy.com>
2024-06-05 14:37:12 +0100

    fix: when turning SqlLog into Log object

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,20 +1,14 @@
 import { SqlLog, LogsResponse } from "../types";
 
-function getCommonFields(log: SqlLog) {
-  return {
+export function getLogResponse(log: SqlLog): LogsResponse {
+  const logResponse: LogsResponse = {
     ...log,
     client_id: log.client_id,
     client_name: "",
     auth0_client: log.auth0_client ? JSON.parse(log.auth0_client) : undefined,
     details: log.details ? JSON.parse(log.details) : undefined,
     isMobile: !!log.isMobile,
-  };
-}
-
-export function getLogResponse(log: SqlLog): LogsResponse {
-  const logResponse: LogsResponse = {
-    ...log,
-    ...getCommonFields(log),
+    scope: log.scope ? log.scope.split(",") : undefined,
     log_id: log.id,
     _id: log.id,
   };
