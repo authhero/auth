dcab8b3351a7eca023d6a06e9dbc94a9a67aafa9
Daniel Gent <dan@sesamy.com>
2024-06-05 12:51:08 +0100

    feat: only one log type

--- src/types/auth0/LogsResponse.ts
+++ src/types/auth0/LogsResponse.ts
@@ -55,9 +55,8 @@ const auth0ClientSchema = z.object({
     })
     .optional(),
 });
-export type Auth0Client = z.infer<typeof auth0ClientSchema>;
 
-const logCommonFieldsSchema = z.object({
+export const logSchema = z.object({
   type: LogType,
   date: z.string(),
   description: z.string().optional(),
@@ -67,137 +66,20 @@ const logCommonFieldsSchema = z.object({
   user_agent: z.string(),
   details: z.any().optional(), // Using z.any() as a placeholder for "details" type
   isMobile: z.boolean(),
-});
-export type LogCommonFields = z.infer<typeof logCommonFieldsSchema>;
-
-const browserLogCommonFieldSchema = logCommonFieldsSchema.extend({
-  user_id: z.string(),
-  user_name: z.string(),
-  // do not have this field yet in SQL
+  //
+  user_id: z.string().optional(),
+  user_name: z.string().optional(),
   connection: z.string().optional(),
-  connection_id: z.string(),
+  connection_id: z.string().optional(),
   client_id: z.string().optional(),
-  client_name: z.string(),
-});
-
-const successfulExchangeOfAccessTokenForAClientCredentialsGrantSchema =
-  browserLogCommonFieldSchema.extend({
-    type: z.literal("seccft"),
-    audience: z.string().optional(),
-    scope: z.union([z.array(z.string()), z.string()]).optional(), // notice how this can be both in auth0! interesting
-    strategy: z.string().optional(),
-    strategy_type: z.string().optional(),
-    hostname: z.string(),
-    auth0_client: auth0ClientSchema,
-  });
-
-const successCrossOriginAuthenticationSchema =
-  browserLogCommonFieldSchema.extend({
-    type: z.literal("scoa"),
-    hostname: z.string(),
-    auth0_client: auth0ClientSchema,
-  });
-export type SuccessCrossOriginAuthentication = z.infer<
-  typeof successCrossOriginAuthenticationSchema
->;
-
-const failedCrossOriginAuthenticationSchema = logCommonFieldsSchema.extend({
-  type: z.literal("fcoa"),
-  hostname: z.string(),
-  connection_id: z.string(),
+  client_name: z.string().optional(),
+  audience: z.string().optional(),
+  scope: z.union([z.array(z.string()), z.string()]).optional(),
+  strategy: z.string().optional(),
+  strategy_type: z.string().optional(),
+  hostname: z.string().optional(),
   auth0_client: auth0ClientSchema,
 });
-export type FailedCrossOriginAuthentication = z.infer<
-  typeof failedCrossOriginAuthenticationSchema
->;
-
-const successApiOperationSchema = logCommonFieldsSchema.extend({
-  type: z.literal("sapi"),
-  client_id: z.string().optional(),
-  client_name: z.string(),
-});
-export type SuccessApiOperation = z.infer<typeof successApiOperationSchema>;
-
-const failedLoginSchema = logCommonFieldsSchema.extend({
-  type: z.literal("f"),
-});
-export type FailedLogin = z.infer<typeof failedLoginSchema>;
-
-const failedLoginIncorrectPasswordSchema = browserLogCommonFieldSchema.extend({
-  type: z.literal("fp"),
-  strategy: z.string(),
-  strategy_type: z.string(),
-});
-export type FailedLoginIncorrectPassword = z.infer<
-  typeof failedLoginIncorrectPasswordSchema
->;
-
-const codeLinkSentSchema = browserLogCommonFieldSchema.extend({
-  type: z.literal("cls"),
-  strategy: z.string(),
-  strategy_type: z.string(),
-});
-export type CodeLinkSent = z.infer<typeof codeLinkSentSchema>;
-
-const failedSilentAuthSchema = logCommonFieldsSchema.extend({
-  type: z.literal("fsa"),
-  hostname: z.string(),
-  audience: z.string(),
-  scope: z.array(z.string()),
-  client_id: z.string().optional(),
-  client_name: z.string(),
-  auth0_client: auth0ClientSchema,
-});
-export type FailedSilentAuth = z.infer<typeof failedSilentAuthSchema>;
-
-const successLogoutSchema = browserLogCommonFieldSchema.extend({
-  type: z.literal("slo"),
-  hostname: z.string(),
-});
-export type SuccessLogout = z.infer<typeof successLogoutSchema>;
-
-const successLoginSchema = browserLogCommonFieldSchema.extend({
-  type: z.literal("s"),
-  strategy: z.string(),
-  strategy_type: z.string(),
-  hostname: z.string(),
-});
-export type SuccessLogin = z.infer<typeof successLoginSchema>;
-
-const successSilentAuthSchema = logCommonFieldsSchema.extend({
-  type: z.literal("ssa"),
-  hostname: z.string(),
-  client_id: z.string().optional(),
-  client_name: z.string(),
-  session_connection: z.string(),
-  user_id: z.string(),
-  user_name: z.string(),
-  auth0_client: auth0ClientSchema,
-});
-export type SuccessSilentAuth = z.infer<typeof successSilentAuthSchema>;
-
-const successSignupSchema = browserLogCommonFieldSchema.extend({
-  type: z.literal("ss"),
-  hostname: z.string(),
-  strategy: z.string(),
-  strategy_type: z.string(),
-});
-export type SuccessSignup = z.infer<typeof successSignupSchema>;
-
-export const logSchema = z.union([
-  successfulExchangeOfAccessTokenForAClientCredentialsGrantSchema,
-  successCrossOriginAuthenticationSchema,
-  successApiOperationSchema,
-  failedLoginIncorrectPasswordSchema,
-  failedCrossOriginAuthenticationSchema,
-  codeLinkSentSchema,
-  failedSilentAuthSchema,
-  successLogoutSchema,
-  successLoginSchema,
-  successSilentAuthSchema,
-  successSignupSchema,
-  failedLoginSchema,
-]);
 
 export type Log = z.infer<typeof logSchema>;
 
