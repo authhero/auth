cbbbb4be7972da5049d7b9a47f96f752f6ba2ef7
Daniel Gent <dan@sesamy.com>
2024-06-05 12:59:03 +0100

    fix: simplify log creation

--- src/adapters/kysely/logs/create.ts
+++ src/adapters/kysely/logs/create.ts
@@ -16,36 +16,6 @@ function flattenScopesIfArray(
   return value;
 }
 
-function getAuth0ClientValue(log: Log): string | undefined {
-  // seems very arbitrary... but it's what auth0 does
-  // const AUTH0_CLIENT_LOG_TYPES = ["seccft", "scoa", "fcoa", "fsa", "ssa"];
-  if (
-    // typescript cannot handle this syntax
-    // AUTH0_CLIENT_LOG_TYPES.includes(log.type) &&
-    log.type === "seccft" ||
-    log.type === "scoa" ||
-    log.type === "fcoa" ||
-    log.type === "fsa" ||
-    log.type === "ssa"
-  ) {
-    return stringifyIfTruthy(log.auth0_client);
-  }
-
-  return undefined;
-}
-
-function getScopeValue(log: Log): string | undefined {
-  if (log.type === "fsa") {
-    return log.scope.join(",");
-  }
-
-  if (log.type === "seccft") {
-    return flattenScopesIfArray(log.scope);
-  }
-
-  return undefined;
-}
-
 export function createLog(db: Kysely<Database>) {
   return async (tenant_id: string, params: Log): Promise<SqlLog> => {
     const { details } = params;
@@ -54,9 +24,9 @@ export function createLog(db: Kysely<Database>) {
       id: nanoid(),
       tenant_id,
       ...params,
-      auth0_client: stringifyIfTruthy(getAuth0ClientValue(params)),
+      auth0_client: stringifyIfTruthy(params.auth0_client),
       details: stringifyIfTruthy(details),
-      scope: getScopeValue(params),
+      scope: flattenScopesIfArray(params.scope),
       isMobile: params.isMobile ? 1 : 0,
     };
     await db.insertInto("logs").values(log).execute();
