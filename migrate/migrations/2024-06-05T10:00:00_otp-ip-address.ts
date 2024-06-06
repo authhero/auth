63dd1d63ee420f58576da6877a93779c21b535cf
Daniel Gent <dan@sesamy.com>
2024-06-06 12:44:53 +0100

    fix: set varchar length

--- migrate/migrations/2024-06-05T10:00:00_otp-ip-address.ts
+++ migrate/migrations/2024-06-05T10:00:00_otp-ip-address.ts
@@ -2,7 +2,7 @@ import { Kysely } from "kysely";
 import { Database } from "../../src/types";
 
 export async function up(db: Kysely<Database>): Promise<void> {
-  await db.schema.alterTable("otps").addColumn("ip", "varchar").execute();
+  await db.schema.alterTable("otps").addColumn("ip", "varchar(64)").execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
