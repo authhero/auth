eb070537398da1ab991efc598c3432c1332262f8
Markus Ahlstrand <markus@ahlstrand.de>
2024-06-08 14:21:00 +0200

    chore: make user use zod

--- src/types/User.ts
+++ src/types/User.ts
@@ -38,28 +38,4 @@ export const auth0UserResponseSchema = userSchema
   })
   .omit({ id: true });
 
-export interface BaseUser {
-  // TODO - Auth0 requires the id OR the email but for our current usage with durable objects and Sesamy's architecture, we need email!
-  email: string;
-  given_name?: string;
-  family_name?: string;
-  nickname?: string;
-  name?: string;
-  picture?: string;
-  locale?: string;
-  linked_to?: string;
-  profileData?: string;
-}
-
-export interface User extends BaseUser {
-  id: string;
-  email_verified: boolean;
-  last_ip?: string;
-  last_login?: string;
-  login_count: number;
-  provider: string;
-  connection: string;
-  is_social: boolean;
-  created_at: string;
-  updated_at: string;
-}
+export type User = z.infer<typeof userSchema>;
