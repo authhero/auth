9e5be1dcc3867eaf78c3b373e0dd4a44ba60e9f5
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-02 15:23:51 +0200

    fix: switch to enter code step

--- test/integration/breakit-customizations.spec.ts
+++ test/integration/breakit-customizations.spec.ts
@@ -135,7 +135,7 @@ test("only allows existing breakit users to progress to the enter code step", as
   // if sign ups are disabled, the create account link should not be shown
   // ----------------------------
 
-  const loginFormNoSignupResponse = await oauthClient.u["enter-email"].$get({
+  const loginFormNoSignupResponse = await oauthClient.u["enter-code"].$get({
     query: {
       state: query.state,
     },
