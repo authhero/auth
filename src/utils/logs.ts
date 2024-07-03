6084483491c776ab03235485ec650112fc198da9
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-03 09:31:59 +0200

    fix: redact headers

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,12 +1,24 @@
 import { SqlLog, LogsResponse } from "../types";
 
+function tryParseJSON(jsonString?: string): any {
+  if (!jsonString) {
+    return "";
+  }
+
+  try {
+    return JSON.parse(jsonString);
+  } catch (e) {
+    return jsonString;
+  }
+}
+
 export function getLogResponse(log: SqlLog): LogsResponse {
   const logResponse: LogsResponse = {
     ...log,
     client_id: log.client_id,
     client_name: "",
-    auth0_client: log.auth0_client ? JSON.parse(log.auth0_client) : undefined,
-    details: log.details ? JSON.parse(log.details) : undefined,
+    auth0_client: tryParseJSON(log.auth0_client),
+    details: tryParseJSON(log.details),
     isMobile: !!log.isMobile,
     scope: log.scope ? log.scope.split(",") : undefined,
     log_id: log.id,
