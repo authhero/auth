71dce72df0e89f87794b09c4b0636c718bf54e45
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-12 15:20:38 +0200

    fix: remove nulls for single tenant

--- src/adapters/kysely/tenants/getTenant.ts
+++ src/adapters/kysely/tenants/getTenant.ts
@@ -1,12 +1,19 @@
 import { Database, Tenant } from "../../../types";
 import { Kysely } from "kysely";
+import { removeNullProperties } from "../helpers/remove-nulls";
 
 export function getTenant(db: Kysely<Database>) {
-  return async (id: string): Promise<Tenant | undefined> => {
-    return db
+  return async (id: string): Promise<Tenant | null> => {
+    const tenant = await db
       .selectFrom("tenants")
       .where("tenants.id", "=", id)
       .selectAll()
       .executeTakeFirst();
+
+    if (!tenant) {
+      return null;
+    }
+
+    return removeNullProperties(tenant);
   };
 }
