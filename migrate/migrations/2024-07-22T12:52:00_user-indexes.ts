40ffa3ab1e517739bdd0b20c88b9966a8f7e14c1
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-22 11:47:32 +0200

    fix: remove old id column

--- migrate/migrations/2024-07-22T12:52:00_user-indexes.ts
+++ migrate/migrations/2024-07-22T12:52:00_user-indexes.ts
@@ -6,10 +6,16 @@ export async function up(db: Kysely<Database>): Promise<void> {
   // ALTER TABLE users DROP PRIMARY KEY;
   // ALTER TABLE users MODIFY user_id VARCHAR(255) NOT NULL;
   // ALTER TABLE users ADD PRIMARY KEY (tenant_id, user_id);
+  // This updated in the initial migration as it doesn't work with sqlite
+  await db.schema.alterTable("users").dropColumn("id").execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
   // Run this is the console to update the user indexes
   // ALTER TABLE users DROP PRIMARY KEY;
   // ALTER TABLE users ADD PRIMARY KEY (tenant_id, id);
+  // await db.schema
+  //   .alterTable("users")
+  //   .addColumn("user_id", "varchar(255)")
+  //   .execute();
 }
