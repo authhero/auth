ceb948e6a52586ef4560d8ed9d099e3df2729678
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-26 15:22:34 +0200

    fix: comment out mysql migration

--- migrate/migrations/2024-07-26T14:18:00_hook-properties.ts
+++ migrate/migrations/2024-07-26T14:18:00_hook-properties.ts
@@ -3,13 +3,13 @@ import { Database } from "@authhero/kysely-adapter";
 
 export async function up(db: Kysely<Database>): Promise<void> {
   // Sqlite does not like this
-  await db.schema
-    .alterTable("hooks")
-    .addColumn("synchronous", "boolean", (col) =>
-      col.defaultTo(false).notNull(),
-    )
-    .addColumn("priority", "integer")
-    .execute();
+  // await db.schema
+  //   .alterTable("hooks")
+  //   .addColumn("synchronous", "boolean", (col) =>
+  //     col.defaultTo(false).notNull(),
+  //   )
+  //   .addColumn("priority", "integer")
+  //   .execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
